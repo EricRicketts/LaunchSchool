5e0d69ac05c2e98a5afac1798a6113ff6be167d8
Eric Ricketts <eric_ricketts@me.com>
2016-10-30 16:00:34 -0400

    use full method notation

--- ProgrammingFoundations/Lesson_2/calculator.rb
+++ ProgrammingFoundations/Lesson_2/calculator.rb
@@ -2,7 +2,7 @@ def get_input
   Kernel.puts("Please input two numbers: ")
   numbers = Kernel.gets.chomp.split.map {|c| c.to_f}
   Kernel.puts("Please input the arithmetic operation (+, -, *, /): ")
-  operation = Kernel.gets.chomp
+  operation = Kernel.gets().chomp()
   return numbers, operation
 end
 
@@ -10,5 +10,5 @@ def perform_operation(numbers, operation)
   numbers[0].send(operation, numbers[1])
 end
 
-numbers, operation = get_input
-puts perform_operation(numbers, operation)
+numbers, operation = get_input()
+Kernel.puts(perform_operation(numbers, operation))
