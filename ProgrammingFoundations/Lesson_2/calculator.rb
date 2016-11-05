882dbfcabf0704a7d6799ff6913937d5a3c1a734
Eric and Wendy Ricketts <EricRicketts@Owners-iPhone.attlocal.net>
2016-11-05 13:34:36 -0400

    test first method in refactored calculator program'

--- ProgrammingFoundations/Lesson_2/calculator.rb
+++ ProgrammingFoundations/Lesson_2/calculator.rb
@@ -1,14 +1,3 @@
-def get_input
-  Kernel.puts("Please input two numbers: ")
-  numbers = Kernel.gets.chomp.split.map {|c| c.to_f}
-  Kernel.puts("Please input the arithmetic operation (+, -, *, /): ")
-  operation = Kernel.gets().chomp()
-  return numbers, operation
+def prompt(message)
+  puts "=> #{message}"
 end
-
-def perform_operation(numbers, operation)
-  numbers[0].send(operation, numbers[1])
-end
-
-numbers, operation = get_input()
-Kernel.puts(perform_operation(numbers, operation))
--- ProgrammingFoundations/Lesson_2/calculator_test.rb
+++ ProgrammingFoundations/Lesson_2/calculator_test.rb
@@ -2,59 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'stringio'
 require_relative './ui'
+require_relative './calculator'
 
-class UIClassTest < Minitest::Test
-
-  def setup()
-    @ui = UI.new
-    @input = StringIO.new("test info\n")
-    @output = StringIO.new
-    @input_received = @ui.receive(stdin: @input)
-    @ui.give("test message", stdout: @output)
+# Test for my own refactoring of the calculator program
+class CalculatorTest < Minitest::Test
+  def test_prompt
+    out = capture_io do
+      prompt("Hello, Jimmy")
+    end
+    assert_equal out[0], "=> Hello, Jimmy\n"
   end
-
-  def test_ui_class_output_and_string_io_overwrites
-    assert_equal "test message\n", @output.string
-  end
-
-  def test_ui_class_input_and_string_io_overwrites
-    assert_equal "test info\n", @input.string
-    assert_equal "test info", @input_received
-  end
-
-end
-
-class InputNumbersAndPerformOperationTest < Minitest::Test
-
-  def setup()
-    @ui = UI.new
-    @output = StringIO.new    
-  end
-
-  def test_ask_for_numbers
-    input = StringIO.new("1 2\n")
-    input_received = @ui.receive(stdin: input)
-    @ui.give("Please input two numbers separated by one or more spaces: ", stdout: @output)
-    assert_equal "Please input two numbers separated by one or more spaces: \n", @output.string
-    assert_equal "1 2", input_received
-    numbers = input_received.split.map {|char| char.to_f} 
-    assert_equal numbers, [1.0, 2.0]
-  end
-
-  def test_ask_for_operation
-    @ui.give("Please type in the arithmetic operation you want on the two numbers (-, +, *, or /): ", stdout: @output)
-    assert_equal "Please type in the arithmetic operation you want on the two numbers (-, +, *, or /): \n", @output.string
-    input = StringIO.new("+\n")
-    input_received = @ui.receive(stdin: input)
-    assert_equal "+", input_received
-  end
-
-  def test_perform_arithmetic_operation
-    numbers = [1.0, 2.0]
-    assert_equal numbers[0].send('+', numbers[1]), 3.0
-    assert_equal numbers[0].send('-', numbers[1]), -1.0
-    assert_equal numbers[0].send('*', numbers[1]), 2.0
-    assert_equal numbers[0].send('/', numbers[1]), 0.5
-  end
-  
 end
