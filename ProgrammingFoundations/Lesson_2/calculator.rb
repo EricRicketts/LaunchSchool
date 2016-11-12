df098412c91c1843975481a67472330b1b00c318
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-11-12 09:16:46 -0500

    alphabetize methods

--- ProgrammingFoundations/Lesson_2/calculator.rb
+++ ProgrammingFoundations/Lesson_2/calculator.rb
@@ -2,65 +2,6 @@ require 'byebug'
 require 'yaml'
 require_relative './constants'
 
-def prompt(message)
-  puts "=> #{message}"
-end
-
-def valid_number?(number)
-  regex = Regexp.new('\A\s*\d+\.?\d*\s*\z')
-  regex.match(number)
-end
-
-def remove_white_space(str)
-  str.strip
-end
-
-def convert_str_to_int_or_float(str)
-  str.to_i == str.to_f ? str.to_i : str.to_f
-end
-
-def name
-  prompt(Constants::WELCOME_MSG)
-  name = ''
-  error_msg = Constants::NAME_ERROR_MSG
-  loop do
-    name = gets.chomp
-    name.empty? ? prompt(error_msg) : break
-  end
-  name
-end
-
-def format_number(msg)
-  number = ''
-  error_msg = Constants::NUMBER_ERROR_MSG
-  loop do
-    prompt(msg)
-    number = gets.chomp
-    valid_number?(number) ? break : prompt(error_msg)
-  end
-  number = remove_white_space(number)
-end
-
-def operation_to_message(operator)
-  Constants::OPERATOR_HASH[operator]
-end
-
-def read_number(msg)
-  number = format_number(msg)
-  convert_str_to_int_or_float(number)
-end
-
-def prompt_for_operation
-  prompt(Constants::OPERATOR_PROMPT)
-  operator = ''
-  operator_choice = Constants::OPERATOR_CHOICE
-  loop do
-    operator = gets.chomp
-    %w(1 2 3 4).include?(operator) ? break : prompt(operator_choice)
-  end
-  operator
-end
-
 def compute_result(operator, num1, num2, msg)
   while num2.zero? && operator == '4'
     prompt(Constants::DIVIDE_BY_ZERO_MSG)
@@ -74,17 +15,78 @@ def compute_result(operator, num1, num2, msg)
   compute_hash[operator]
 end
 
+def convert_str_to_int_or_float(str)
+  str.to_i == str.to_f ? str.to_i : str.to_f
+end
+
+def format_number(msg)
+  number = ''
+  error_msg = Constants::NUMBER_ERROR_MSG
+  loop do
+    prompt(msg)
+    number = gets.chomp
+    valid_number?(number) ? break : prompt(error_msg)
+  end
+  number = remove_white_space(number)
+end
+
+def name
+  prompt(Constants::WELCOME_MSG)
+  name = ''
+  loop do
+    name = gets.chomp
+    name.empty? ? prompt(Constants::NAME_ERROR_MSG) : break
+  end
+  name
+end
+
+def operation_to_message(operator)
+  Constants::OPERATOR_HASH[operator]
+end
+
+def print_result(result)
+  prompt("The result is #{result}")
+  prompt(Constants::REPEAT_CALC_MSG)
+end
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def prompt_for_operation
+  prompt(Constants::OPERATOR_PROMPT)
+  operator = ''
+  operator_choice = Constants::OPERATOR_CHOICE
+  loop do
+    operator = gets.chomp
+    %w(1 2 3 4).include?(operator) ? break : prompt(operator_choice)
+  end
+  operator
+end
+
 def read_in_numbers
   num1 = read_number(Constants::FIRST_NUMBER_MSG)
   num2 = read_number(Constants::SECOND_NUMBER_MSG)
   [num1, num2]
 end
 
-def print_result_and_repeat(result)
-  prompt("The result is #{result}")
-  prompt(Constants::REPEAT_CALC_MSG)
+def read_number(msg)
+  number = format_number(msg)
+  convert_str_to_int_or_float(number)
+end
+
+def remove_white_space(str)
+  str.strip
+end
+
+def repeat?
   answer = gets.chomp
   answer.downcase().start_with?('y') ? true : false
+end
+
+def valid_number?(number)
+  regex = Regexp.new('\A\s*\d+\.?\d*\s*\z')
+  regex.match(number)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -94,8 +96,8 @@ if __FILE__ == $PROGRAM_NAME
     operator = prompt_for_operation
     result = compute_result(operator, number1, \
                             number2, Constants::SECOND_NUMBER_MSG)
-    repeat = print_result_and_repeat(result)
-    break unless repeat
+    print_result(result)
+    break unless repeat?
   end # main loop
   prompt(Constants::GOOD_BYE_MSG)
 end
