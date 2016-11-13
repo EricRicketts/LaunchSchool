7bb20c379d89a1b2c52a46d6417b91afa303dc35
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-11-12 21:41:04 -0500

    start coding first part of final program

--- ProgrammingFoundations/Lesson_2/mortgate_calculator/mortgage_calculator.rb
+++ ProgrammingFoundations/Lesson_2/mortgate_calculator/mortgage_calculator.rb
@@ -5,7 +5,11 @@ APP_CONFIG = YAML.load(raw_config)
 
 def convert_input_to_float(stdin: $stdin)
   input = stdin.gets.chomp
-  input.delete!(",").to_f
+  until valid_number?(input)
+    puts invalid_number_msg
+    input = stdin.gets.chomp
+  end
+  input.delete(",").to_f
 end
 
 def invalid_number_msg
@@ -32,3 +36,11 @@ end
 def welcome_message
   prompt(APP_CONFIG['WelcomeMsg'])
 end
+
+if __FILE__ == $PROGRAM_NAME
+  puts welcome_message
+  puts loan_amt_message
+  loan_amount = convert_input_to_float
+  puts loan_amount
+end
+
