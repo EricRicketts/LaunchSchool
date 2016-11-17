c61b84be1f062f908b3a927b09954e06b8d10723
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-11-17 04:45:26 -0500

    calculator_constants_test fixed now all string constants from config.yml are tested

--- ProgrammingFoundations/Lesson_2/my_calculator_solution/calculator_constants_test.rb
+++ ProgrammingFoundations/Lesson_2/my_calculator_solution/calculator_constants_test.rb
@@ -1,27 +1,63 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative './constants'
+require 'yaml'
+
+raw_config = File.read('./config.yml')
+APP_CONFIG = YAML.load(raw_config)
 
 # making sure constants module works
 class CalculatorConstantsTest < Minitest::Test
-  def setup
-    @operator_prompt = "What operation would you like to perform? " \
-    "(enter a number)\n" \
+  def test_first_number_message
+    assert_equal "What's the first number?", APP_CONFIG['FirstNumberMsg']
+  end
+
+  def test_second_number_message
+    assert_equal "What's the second number?", APP_CONFIG['SecondNumberMsg']
+  end
+
+  def test_welcome_message
+    msg = "Welcome to Calculator!  Enter your name: "
+    assert_equal msg, APP_CONFIG['WelcomeMsg']
+  end
+
+  def test_goodbye_message
+    msg = "Thank you for using the calculator.  Good bye!"
+    assert_equal msg, APP_CONFIG['GoodByeMsg']
+  end
+
+  def test_name_error_message
+    msg = "Make sure to use a valid name."
+    assert_equal msg, APP_CONFIG['NameErrorMsg']
+  end
+
+  def test_operator_choice_message
+    msg = "Must choose 1, 2, 3, or 4"
+    assert_equal msg, APP_CONFIG['OperatorChoiceMsg']
+  end
+
+  def test_divide_by_zero_message
+    msg = "Oops!!  Cannot divide by zero, choose another number ..."
+    assert_equal msg, APP_CONFIG['DivideByZeroMsg']
+  end
+
+  def test_incorrect_message
+    msg = "Oops!!  I don't understand please input " \
+    "(Y/y) to continue or (N/n) to quit."
+    assert_equal msg, APP_CONFIG['IncorrectMsg']
+  end
+
+  def test_operator_prompt_message
+    msg = "What operation would you like to perform? (enter a number)\n" \
     "1) Add\n" \
     "2) Subtract\n" \
     "3) Multiply\n" \
-    "4) Divide"
+    "4) Divide\n"
+    assert_equal msg, APP_CONFIG['OperatorPrompt']
   end
 
-  def test_first_number_message
-    assert_equal "What's the first number?", Constants::FIRST_NUMBER_MSG
-  end
-
-  def test_operator_hash
-    assert_equal 'Multiplying', Constants::OPERATOR_HASH['3']
-  end
-
-  def test_operator_prompt
-    assert_equal @operator_prompt, Constants::OPERATOR_PROMPT
+  def test_repeat_calculation_message
+    msg = "Do you want to perform another calculation?\n" \
+    "(Y/y to calculate again or N/n to quit)\n`"
+    assert_equal msg, APP_CONFIG['RepeatCalculationMsg']
   end
 end
