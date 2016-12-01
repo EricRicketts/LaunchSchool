b0280f595bb2c5acafa9f8926bbf65e10442ff11
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-12-01 05:49:28 -0500

    questions five and six for easy three

--- ProgrammingFoundations/lesson_3_exercises/easy_three/question_four.rb
+++ ProgrammingFoundations/lesson_3_exercises/easy_three/question_four.rb
@@ -10,4 +10,13 @@ class QuestionFour < Minitest::Test
     assert_equal expected_slice, sliced_string
     assert_equal expected, advice
   end
+
+  def test_shorten_string_another_way
+    advice = "Few things in life are as important as house training your pet dinosaur."
+    expected = "house training your pet dinosaur."
+    expected_slice = "Few things in life are as important as "
+    sliced_string = advice.slice!(0, advice.index("house"))
+    assert_equal expected_slice, sliced_string
+    assert_equal expected, advice
+  end
 end
