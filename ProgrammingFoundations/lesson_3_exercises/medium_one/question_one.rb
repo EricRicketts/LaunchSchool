12b0527c45b342b75fa0e2c53afb70fd893f8a6b
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-12-01 20:18:46 -0500

    question one medium one exercises

---
+++ ProgrammingFoundations/lesson_3_exercises/medium_one/question_one.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+=begin
+Let's do some "ASCII Art" (a stone-age form of nerd artwork from back in the days before computers had video screens).
+For this exercise, write a one-line program that creates the following output 10 times, with the subsequent line indented 1 space to the right:
+The Flintstones Rock!
+ The Flintstones Rock!
+  The Flintstones Rock!
+=end
+class QuestionOne < Minitest::Test
+  def test_indentations
+    base_string = "The Flintstones Rock!"
+    expected = "\s" * 0 + "#{base_string}" + "\n" +
+               "\s" * 1 + "#{base_string}" + "\n" +
+               "\s" * 2 + "#{base_string}" + "\n" +
+               "\s" * 3 + "#{base_string}" + "\n" +
+               "\s" * 4 + "#{base_string}" + "\n" +
+               "\s" * 5 + "#{base_string}" + "\n" +
+               "\s" * 6 + "#{base_string}" + "\n" +
+               "\s" * 7 + "#{base_string}" + "\n" +
+               "\s" * 8 + "#{base_string}" + "\n" +
+               "\s" * 9 + "#{base_string}" + "\n"
+    # 10.times { |n| puts base_string.rjust(base_string.length + n) }
+    output = capture_io do
+      10.times { |n| puts base_string.rjust(base_string.length + n) }
+    end
+    output.pop
+    assert_equal expected, output[0]
+  end
+end
