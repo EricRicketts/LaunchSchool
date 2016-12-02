90241ce4855cd931e6ea24a64dbc1763469e4558
Eric and Wendy Ricketts <eric_ricketts@me.com>
2016-12-02 07:12:08 -0500

    a much more satisfying answer for question two medium one exercises

--- ProgrammingFoundations/lesson_3_exercises/medium_one/question_two.rb
+++ ProgrammingFoundations/lesson_3_exercises/medium_one/question_two.rb
@@ -2,21 +2,30 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'byebug'
 class QuestionTwo < Minitest::Test
-  def test_add_to_hash
-    expected = {
+  def setup
+    @statement = "The Flintstones Rock"
+    @expected = {
       "T" => 1, "h" => 1, "e" => 2, "F" => 1, "l" => 1, "i" => 1,
       "n" => 2, "t" => 2, "s" => 2, "o" => 2, "R" => 1, "c" => 1,
       "k" => 1
     }
-    statement = "The Flintstones Rock"
+  end
+
+  def test_add_to_hash
     hash = {}
-    statement.gsub(/\s+/, "").chars.each do |char|
+    @statement.gsub(/\s+/, "").chars.each do |char|
       if hash.keys.include?(char)
         hash[char] += 1
       else
         hash[char] = 1
       end
     end
-    assert_equal expected, hash
+    assert_equal @expected, hash
+  end
+
+  def test_another_way
+    hash = {}
+    @statement.gsub!(/\s+/,"").chars {|char| hash[char] = @statement.count(char)}
+    assert_equal @expected, hash
   end
 end
