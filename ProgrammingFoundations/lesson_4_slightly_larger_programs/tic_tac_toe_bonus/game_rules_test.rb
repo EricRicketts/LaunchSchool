6f30e00847fad75f1d9126d69b32cf679824dc71
Eric and Wendy Ricketts <eric_ricketts@me.com>
2017-01-11 06:21:28 -0500

    moved both diagonal and anti-diagonal methods into game_rules module

--- ProgrammingFoundations/lesson_4_slightly_larger_programs/tic_tac_toe_bonus/game_rules_test.rb
+++ ProgrammingFoundations/lesson_4_slightly_larger_programs/tic_tac_toe_bonus/game_rules_test.rb
@@ -22,4 +22,90 @@ class TestThreeInARowWins < Minitest::Test
     selected_square = 6
     assert_nil detect_row_winner(@board, selected_square, player)
   end
+
+ def test_win_on_a_column
+    @board[0][2] = @board[1][2] = @board[2][2] = "O"
+    player = "O"
+    selected_square = 9
+    assert_equal "O", detect_column_winner(@board, selected_square, player)
+  end
+
+  def test_no_win_on_a_column
+    @board[0][2] = @board[2][2] = "X"
+    @board[1][2] = "O"
+    player = "X"
+    selected_square = 9
+    assert_nil detect_column_winner(@board, selected_square, player)
+  end
+
+  def test_generate_valid_diagonal_squares_tic_tac_toe
+    expected_squares = [1, 5, 9]
+    assert_equal expected_squares, generate_diagonal_square_numbers(@board)
+  end
+
+  def test_generate_valid_diagonal_squares_larger_tic_tac_toe
+    large_board = Array.new(5) { Array.new(5, "\u0020") }
+    expected_squares = [1, 7, 13, 19, 25]
+    assert_equal expected_squares, generate_diagonal_square_numbers(large_board)
+  end
+
+  def test_win_on_a_diagonal
+    @board[0][0] = @board[1][1] = @board[2][2] = "O"
+    player = "O"
+    selected_square = 5
+    assert_equal "O", detect_diagonal_winner(@board, selected_square, player)
+  end
+
+  def test_no_square_on_a_diagonal
+    @board[0][0] = @board[2][2] = "X"
+    @board[1][1] = "O"
+    player = "X"
+    selected_square = 2
+    assert_nil detect_diagonal_winner(@board, selected_square, player)
+  end
+
+  def test_no_win_on_a_diagonal
+    @board[0][0] = @board[2][2] = "X"
+    @board[1][1] = "O"
+    player = "O"
+    selected_square = 5
+    assert_nil detect_diagonal_winner(@board, selected_square, player)
+  end
+
+  def test_generate_valid_anti_diagonal_squares_tic_tac_toe
+    expected_squares = [3, 5, 7]
+    actual_squares = generate_anti_diagonal_square_numbers(@board)
+    assert_equal expected_squares, actual_squares
+  end
+
+  def test_generate_valid_anti_diagonal_squares_larger_tic_tac_toe
+    large_board = Array.new(5) { Array.new(5, "\u0020") }
+    expected_squares = [5, 9, 13, 17, 21]
+    actual_squares = generate_anti_diagonal_square_numbers(large_board)
+    assert_equal expected_squares, actual_squares
+  end
+
+  def test_win_on_an_anti_diagonal
+    @board[0][2] = @board[1][1] = @board[2][0] = "O"
+    player = expected = "O"
+    selected_square = 7
+    actual = detect_anti_diagonal_winner(@board, selected_square, player)
+    assert_equal expected, actual
+  end
+
+  def test_no_square_on_an_anti_diagonal
+    @board[0][2] = @board[2][0] = "X"
+    @board[1][1] = "O"
+    player = "X"
+    selected_square = 8
+    assert_nil detect_anti_diagonal_winner(@board, selected_square, player)
+  end
+
+  def test_no_win_on_an_anti_diagonal
+    @board[0][2] = @board[2][0] = "X"
+    @board[1][1] = "O"
+    player = "O"
+    selected_square = 5
+    assert_nil detect_anti_diagonal_winner(@board, selected_square, player)
+  end
 end # TestThreeInARowWins
--- ProgrammingFoundations/lesson_4_slightly_larger_programs/tic_tac_toe_bonus/tic_tac_toe_bonus.rb
+++ ProgrammingFoundations/lesson_4_slightly_larger_programs/tic_tac_toe_bonus/tic_tac_toe_bonus.rb
@@ -44,12 +44,12 @@ def declare_winner_and_update_tally(board, selected_square,
   possible_winning_plays = [
     GameRules.detect_row_winner(
       board, selected_square, player_symbols[current_player]),
-    detect_column_winner(board, selected_square,
-                         player_symbols[current_player]),
-    detect_diagonal_winner(board, selected_square,
-                           player_symbols[current_player]),
-    detect_anti_diagonal_winner(board, selected_square,
-                                player_symbols[current_player])
+    GameRules.detect_column_winner(board, selected_square,
+                                   player_symbols[current_player]),
+    GameRules.detect_diagonal_winner(board, selected_square,
+                                     player_symbols[current_player]),
+    GameRules.detect_anti_diagonal_winner(board, selected_square,
+                                          player_symbols[current_player])
   ]
   increment_tally_and_output_winner_string(possible_winning_plays,
                                            player_symbols, tally)
@@ -59,40 +59,6 @@ def decrement(num)
   num - 1
 end
 
-def detect_anti_diagonal_winner(board, selected_square, player)
-  row_size = board.first.size
-  anti_diagonal_square_numbers = generate_anti_diagonal_square_numbers(board)
-  return nil unless anti_diagonal_square_numbers.include?(selected_square)
-
-  winner = generate_and_check_either_diagonal(anti_diagonal_square_numbers,
-                                              board, row_size, player)
-  winner ? player : nil
-end
-
-def detect_column_winner(board, selected_square, player)
-  column = decrement(selected_square) % 3
-  column_values = board[0..2].map { |row| row[column] }
-  winner = column_values.all? { |square| square.eql?(player) }
-  winner ? player : nil
-end
-
-def detect_diagonal_winner(board, selected_square, player)
-  row_size = board.first.size
-  diagonal_square_numbers = generate_diagonal_square_numbers(board)
-  return nil unless diagonal_square_numbers.include?(selected_square)
-
-  winner = generate_and_check_either_diagonal(diagonal_square_numbers,
-                                              board, row_size, player)
-  winner ? player : nil
-end
-
-# def GameRules.detect_row_winner(board, selected_square, player)
-#   row_size = board.first.size
-#   row = decrement(selected_square).div(row_size)
-#   winner = board[row].all? { |square| square.eql?(player) }
-#   winner ? player : nil
-# end
-
 def do_not_play_again?
   puts GameText.ask_for_another_game
   answer = gets.chomp.strip.upcase
@@ -101,30 +67,6 @@ def do_not_play_again?
     answer = gets.chomp.strip.upcase
   end
   answer.eql?("N")
-end
-
-def generate_and_check_either_diagonal(square_numbers, board, row_size, player)
-  decremented_square_numbers =
-    square_numbers.map { |square_number| decrement(square_number) }
-  either_diagonal_squares =
-    decremented_square_numbers.map { |square| square.divmod(row_size) }
-  either_diagonal_squares.all? do |square|
-    board[square.first][square.last].eql?(player)
-  end
-end
-
-def generate_anti_diagonal_square_numbers(board)
-  row_size = board.first.size
-  (2..row_size).to_a.inject([row_size]) do |anti_diagonal_numbers|
-    anti_diagonal_numbers << anti_diagonal_numbers.last + row_size - 1
-  end
-end
-
-def generate_diagonal_square_numbers(board)
-  row_size = board.first.size
-  (2..row_size).to_a.inject([1]) do |diagonal_numbers|
-    diagonal_numbers << diagonal_numbers.last + row_size + 1
-  end
 end
 
 def increment_tally_and_output_winner_string(possible_winning_plays,
@@ -273,9 +215,9 @@ end
 
 def win_or_tie?(board, selected_square, player)
   !!GameRules.detect_row_winner(board, selected_square, player) ||
-    !!detect_column_winner(board, selected_square, player) ||
-    !!detect_diagonal_winner(board, selected_square, player) ||
-    !!detect_anti_diagonal_winner(board, selected_square, player) ||
+    !!GameRules.detect_column_winner(board, selected_square, player) ||
+    !!GameRules.detect_diagonal_winner(board, selected_square, player) ||
+    !!GameRules.detect_anti_diagonal_winner(board, selected_square, player) ||
     tie?(board)
 end
 
