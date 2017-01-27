2a56273cd9c0a5ce50ddc451225b534e3e387fbb
Eric and Wendy Ricketts <eric_ricketts@me.com>
2017-01-26 20:35:04 -0500

    add information onto exercise 12

--- ProgrammingFoundations/lesson_4_ruby_collections/methods_more_methods_and_sorting/exercise_12_test.rb
+++ ProgrammingFoundations/lesson_4_ruby_collections/methods_more_methods_and_sorting/exercise_12_test.rb
@@ -18,4 +18,21 @@ class Exercise12Test < Minitest::Test
     ]
     assert_equal expected, result
   end
+
+  def test_order_array_of_hashes_a_better_way
+    books = [
+      {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
+      {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
+      {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
+      {title: 'Ulysees', author: 'James Joyce', published: '1922'}
+    ]
+    result = books.sort_by { |book| book[:published] }
+    expected = [
+      {:title=>"War and Peace", :author=>"Leo Tolstoy", :published=>"1869"},
+      {:title=>"Ulysees", :author=>"James Joyce", :published=>"1922"},
+      {:title=>"The Great Gatsby", :author=>"F. Scott Fitzgerald", :published=>"1925"}, 
+      {:title=>"One Hundred Years of Solitude", :author=>"Gabriel Garcia Marquez", :published=>"1967"}
+    ]
+    assert_equal expected, result
+  end
 end
