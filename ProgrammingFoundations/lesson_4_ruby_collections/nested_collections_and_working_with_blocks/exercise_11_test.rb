30562de8384db7d919423e910e285378c6f53d9f
Eric and Wendy Ricketts <eric_ricketts@me.com>
2017-01-31 15:31:18 -0500

    exercise twelve

--- ProgrammingFoundations/lesson_4_ruby_collections/nested_collections_and_working_with_blocks/exercise_11_test.rb
+++ ProgrammingFoundations/lesson_4_ruby_collections/nested_collections_and_working_with_blocks/exercise_11_test.rb
@@ -5,6 +5,7 @@ class Exercise11Test < Minitest::Test
   def test_pick_evens
     arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
     expected = [{ e: [8], f: [6, 10] }]
+
     result = []
     arr.each do |hsh|
       result.push(hsh) if hsh.values.flatten.all?(&:even?)
