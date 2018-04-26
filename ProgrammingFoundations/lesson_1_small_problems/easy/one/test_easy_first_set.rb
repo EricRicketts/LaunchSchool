273997749a9f9965ce5885f2a5a9477025a3091b
EricRicketts <eric_ricketts@me.com>
2018-04-26 16:11:22 -0400

    I wrote a better test to capture the exact intent of the first exercise from the easy one problem set.

--- ProgrammingFoundations/lesson_1_small_problems/easy/one/test_easy_first_set.rb
+++ ProgrammingFoundations/lesson_1_small_problems/easy/one/test_easy_first_set.rb
@@ -5,12 +5,14 @@ require 'byebug'
 class TestEasyFirstSet < Minitest::Test
 
   def repeat(str, reps)
-    str*reps
+    reps.times do
+      puts str
+    end
   end
 
   def test_repeat_yourself_1
     expected = "Hello\nHello\nHello\n"
-    assert_equal(expected, repeat("Hello\n", 3))
+    assert_output(expected) { repeat("Hello", 3) }
   end
   
 end
