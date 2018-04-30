6ffd913651e43ab4cfeb1c6e34546e6ef50ee51a
EricRicketts <eric_ricketts@me.com>
2018-04-30 11:34:31 -0400

    reverse odd words problem, my solution

--- ProgrammingFoundations/lesson_1_small_problems/easy/one/test_easy_first_set.rb
+++ ProgrammingFoundations/lesson_1_small_problems/easy/one/test_easy_first_set.rb
@@ -10,9 +10,24 @@ class TestEasyFirstSet < Minitest::Test
     end
   end
 
-  def test_repeat_yourself_1
+  def is_odd?(int)
+    # int.abs % 2 == 1
+    # above solution requires the
+    # #abs to ensure result is 1 or 0
+    # below any even number, positive or
+    # negative modulus 2 is zero. 
+    !(int % 2).zero?
+  end
+
+  def test_1_repeat_yourself
     expected = "Hello\nHello\nHello\n"
     assert_output(expected) { repeat("Hello", 3) }
+  end
+
+  def test_2_odd
+    odd_arr = [5, -17, 7]
+    even_arr = [2, -8, 0]
+    assert(odd_arr.all? {|n| is_odd?(n)} && even_arr.none? {|n| is_odd?(n)})
   end
   
 end
