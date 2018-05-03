167fe38bc70f783ca0cf4177eb1ca7e5ec6f759b
EricRicketts <eric_ricketts@me.com>
2018-05-03 17:37:01 -0400

    fourth exercise easy one

---
+++ ProgrammingFoundations/lesson_1_small_problems/easy/one/fourth_exercise.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'byebug'
+
+class FourthExercise < Minitest::Test
+
+=begin
+Write a method that counts the number of occurrences of each element in a given array.
+
+vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
+
+count_occurrences(vehicles)
+Once counted, print each element alongside the number of occurrences.
+
+Expected output:
+
+car => 4
+truck => 3
+SUV => 1
+motorcycle => 2
+
+Input: array of strings
+Output: Hash
+
+Algorithm:
+
+  - initialize an empty hash with 0 value Occurances = Hash.new(0)
+  - cycle through each word in the array
+    - for each word use it as a key in the occurances hash and increment the value by 1
+=end
+
+  def count_occurrences(ary)
+    hsh = Hash.new(0)
+    ary.each {|word| hsh[word] += 1}
+    hsh
+  end
+
+
+  def test_count_words
+    expected = { 'car' => 4, 'truck' => 3, 'SUV' => 1, 'motorcycle' => 2}
+    vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle',
+      'motorcycle', 'car', 'truck']
+    assert_equal(expected, count_occurrences(vehicles))    
+  end
+
+end
