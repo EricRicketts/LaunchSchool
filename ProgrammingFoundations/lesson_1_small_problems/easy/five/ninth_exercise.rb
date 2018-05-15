824b1004bbd6d32934a38037351d102e290845d0
EricRicketts <eric_ricketts@me.com>
2018-05-15 14:52:49 -0400

    ninth exercise Small Problems Easy Set Five and Session Four Problems from Study Group

--- ProgrammingFoundations/lesson_1_small_problems/easy/five/ninth_exercise.rb
+++ ProgrammingFoundations/lesson_1_small_problems/easy/five/ninth_exercise.rb
@@ -15,11 +15,24 @@ crunch('4444abcabccba') == '4abcabcba'
 crunch('ggggggggggggggg') == 'g'
 crunch('a') == 'a'
 crunch('') == ''
+
+
+I: string
+O: collapsed string
+
+AL:
+  - split into words
+    - split each word into a character array
+      - set previous char to ''
+      - if the current char == previous char shift the array
+      - at the end join the array
+    
 =end
 
 class NinthExercise < Minitest::Test
 
   def crunch(str)
+     str.gsub(/([[:alnum:]])(\1)+/, '\1')
   end
 
   def test_1
@@ -41,5 +54,80 @@ class NinthExercise < Minitest::Test
   def test_5
     assert_equal('', crunch(''))    
   end
+  
+end
+
+class NinthExerciseVersionTwo < Minitest::Test
+
+  def crunch(str)
+    str_ary = str.split
+    str_ary.map do |word|
+      letter_ary = word.chars
+      letter_ary.reject.with_index do |char, idx|
+        next if idx.zero?
+        char == letter_ary[idx - 1]
+      end.join
+    end.join(' ') 
+  end
+
+  def test_1
+    assert_equal('daily double', crunch('ddaaiillyy ddoouubbllee'))    
+  end
+  
+  def test_2
+    assert_equal('4abcabcba', crunch('4444abcabccba'))    
+  end
+  
+  def test_3
+    assert_equal('g', crunch('ggggggggggggggg'))    
+  end
+  
+  def test_4
+    assert_equal('a', crunch('a'))    
+  end
+  
+  def test_5
+    assert_equal('', crunch(''))    
+  end  
+  
+end
+
+class NinthExerciseLS < Minitest::Test
+
+=begin
+I like this solution for its simplicity, it traversed through the index
+of the string and just pushed the correct character onto a new string
+if it DID NOT MATCH the next character in the sequence
+=end
+
+  def crunch(str)
+    crunched_str = ''
+    idx = 0
+    while idx < str.length
+      crunched_str << str[idx] unless str[idx] == str[idx + 1]
+      idx += 1
+    end
+    crunched_str
+  end
+
+  def test_1
+    assert_equal('daily double', crunch('ddaaiillyy ddoouubbllee'))    
+  end
+  
+  def test_2
+    assert_equal('4abcabcba', crunch('4444abcabccba'))    
+  end
+  
+  def test_3
+    assert_equal('g', crunch('ggggggggggggggg'))    
+  end
+  
+  def test_4
+    assert_equal('a', crunch('a'))    
+  end
+  
+  def test_5
+    assert_equal('', crunch(''))    
+  end  
   
 end
