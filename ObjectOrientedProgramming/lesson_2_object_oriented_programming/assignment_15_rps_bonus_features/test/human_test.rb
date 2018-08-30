f495b0e811e9181046dd9597e007159cf8e1b99e
EricRicketts <eric_ricketts@me.com>
2018-08-30 05:17:43 -0400

    human name loop, correct entry for human chooses move

---
+++ ObjectOrientedProgramming/lesson_2_object_oriented_programming/assignment_15_rps_bonus_features/test/human_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry-byebug'
+
+require_relative '../lib/human'
+
+class HumanTest < Minitest::Test
+  attr_accessor :io
+
+  def setup
+    @io = StringIO.new
+    $stdin = io
+  end
+
+  def teardown
+    io.rewind
+    $stdin = STDIN
+  end
+
+  def test_set_name
+    io.string = "Foo\n"
+    human = ''
+    assert_output(/Please enter your name: \n/) { human = Human.new }
+    assert_equal("Foo", human.name)
+  end
+
+  def test_set_name_after_erred_input
+    expected = "Please enter your name: \n" +
+      "Sorry, you must enter a value for your name.\n" +
+      "Please enter your name: \n"
+    human = ''
+    out, err = capture_io do
+      io.string = "\nFoo\n"
+      human = Human.new
+    end
+    assert_equal(expected, out)
+    assert_equal("Foo", human.name)
+  end
+
+  def test_choose_move
+    expected = /Please choose rock, paper, scissors, lizard, or spock:/
+    io.string = "Foo\nrock\n"
+    human = ''
+    assert_output(expected) { human = Human.new; human.choose }
+    assert_instance_of(Rock, human.move.selection)
+  end
+
+end
