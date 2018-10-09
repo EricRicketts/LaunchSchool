0d67c4c807405821a34b92a9c43684465ae2380e
EricRicketts <eric_ricketts@me.com>
2018-10-09 15:41:04 -0400

    basics inheritance exercises seven and eight

---
+++ exercises/object_oriented_programming/basics/inheritance/exercise_seven_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry-byebug'
+
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+$ancestors = Cat.ancestors
+$ancestors.delete(PP::ObjectMixin)
+$ancestors.delete(Minitest::Expectations)
+
+
+class ExerciseSevenTest < Minitest::Test
+  def test_one
+    cat = Cat.new('Black')
+    assert_equal(Animal, cat.method(:color).owner)
+  end
+
+  def test_two
+    expected = [
+      Cat, Animal, Object, Kernel, BasicObject
+    ]
+    assert_equal(expected, $ancestors)
+  end
+end
