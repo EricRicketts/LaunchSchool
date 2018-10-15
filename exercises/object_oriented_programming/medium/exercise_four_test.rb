261b8ca99ee8bbbf7f9826c616f84cacbeba9f23
EricRicketts <eric_ricketts@me.com>
2018-10-15 07:20:54 -0400

    medium exercise four

--- exercises/object_oriented_programming/medium/exercise_four_test.rb
+++ exercises/object_oriented_programming/medium/exercise_four_test.rb
@@ -3,64 +3,88 @@ require 'minitest/pride'
 require 'pry-byebug'
 
 class ExerciseFourTest < Minitest::Test
-  class CircularQueue
-    attr_reader :size
+  attr_accessor :queue
 
-    def initialize(size)
-      @size = size
-      @queue = Array.new(size)
+  class CircularQueue
+    attr_reader :max_size
+
+    def initialize(max_size)
+      @max_size = max_size
+      @q = []
     end
 
     def dequeue
-
+      @q.pop
     end
 
     def enqueue(n)
-      @queue = @queue.unshift(n).take(size)
+      @q = full? ? @q.unshift(n).take(max_size) : @q.unshift(n)
+    end
+
+    private
+
+    def full?
+      max_size == @q.size
     end
   end
 
+  def setup
+    @queue = CircularQueue.new(3)
+  end
+
   def test_enqueue_one_element
-    queue = CircularQueue.new(3)
     queue.enqueue(1)
-    expected = [1, nil, nil]
-    results = queue.instance_variable_get("@queue")
+    expected = [1]
+    results = queue.instance_variable_get("@q")
     assert_equal(expected, results)
   end
 
   def test_enqueue_two_elements
-    queue = CircularQueue.new(3)
     queue.enqueue(2)
     queue.enqueue(1)
-    expected = [1, 2, nil]
-    results = queue.instance_variable_get("@queue")
+    expected = [1, 2]
+    results = queue.instance_variable_get("@q")
     assert_equal(expected, results)
   end
 
   def test_enqueue_three_elements
-    queue = CircularQueue.new(3)
     queue.enqueue(3)
     queue.enqueue(2)
     queue.enqueue(1)
     expected = [1, 2, 3]
-    results = queue.instance_variable_get("@queue")
+    results = queue.instance_variable_get("@q")
     assert_equal(expected, results)
   end
 
   def test_enqueue_full_queue
-    queue = CircularQueue.new(3)
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(3)
     queue.enqueue(4)
     expected = [4, 3, 2]
-    results = queue.instance_variable_get("@queue")
+    results = queue.instance_variable_get("@q")
+    assert_equal(expected, results)
+  end
+
+  def test_dequeue_one_element_queue
+    queue.enqueue(1)
+    expected = [1, []]
+    dq_result = queue.dequeue
+    results = [dq_result, queue.instance_variable_get("@q")]
+    assert_equal(expected, results)
+  end
+
+  def test_dequeue_full_queue
+    queue.enqueue(3)
+    queue.enqueue(2)
+    queue.enqueue(1)
+    expected = [3, [1, 2]]
+    dq_result = queue.dequeue
+    results = [dq_result, queue.instance_variable_get("@q")]
     assert_equal(expected, results)
   end
 
   def test_final
-    skip
-    queue = CircularQueue.new(3)
     assert(queue.dequeue == nil)
 
     queue.enqueue(1)
