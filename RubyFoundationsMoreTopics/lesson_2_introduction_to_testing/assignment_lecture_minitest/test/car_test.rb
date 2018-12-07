37e15dd8cfc0970eb6fe3e18f0319005b53d26db
EricRicketts <eric_ricketts@me.com>
2018-12-07 07:03:52 -0700

    skip a test

--- RubyFoundationsMoreTopics/lesson_2_introduction_to_testing/assignment_lecture_minitest/test/car_test.rb
+++ RubyFoundationsMoreTopics/lesson_2_introduction_to_testing/assignment_lecture_minitest/test/car_test.rb
@@ -1,10 +1,18 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
 require_relative '../code/car'
+Minitest::Reporters.use!
 
 class CarTest < Minitest::Test
 
   def test_wheels
     car = Car.new
     assert_equal(4, car.wheels)
+  end
+
+  def test_bad_wheels
+    skip
+    car = Car.new
+    assert_equal(3, car.wheels)
   end
 end
