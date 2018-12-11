e161cbdfcf116acd58b35a0cd55e7ec4bbe46b9e
EricRicketts <eric_ricketts@me.com>
2018-12-11 18:39:21 -0500

    more topics, easy one, exercise one

--- anki/anki.rb
+++ anki/anki.rb
@@ -2,4 +2,23 @@
 # require 'minitest/pride'
 require 'pry-byebug'
 
-puts [2, 3, 5].inject(1, &:+)
+require 'minitest/autorun'
+
+class Cat
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class AnkiTest < Minitest::Test
+  attr_accessor :kitty
+  def setup
+    @kitty = Cat.new('Kitty')
+  end
+
+  def test_foo
+    assert(kitty.name == 'Milo')
+  end
+end
