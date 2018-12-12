232af0e52fec557df592f734bd64db0af5d9c879
EricRicketts <eric_ricketts@me.com>
2018-12-12 11:13:06 -0500

    added two more solutions to more topics, exercises, exercise 3, explained the Launch School solution

--- anki/anki.rb
+++ anki/anki.rb
@@ -3,22 +3,36 @@
 require 'pry-byebug'
 
 require 'minitest/autorun'
+require 'minitest/pride'
 
-class Cat
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-end
 
 class AnkiTest < Minitest::Test
-  attr_accessor :kitty
+  attr_accessor :is_even
+
   def setup
-    @kitty = Cat.new('Kitty')
+    @is_even = lambda { |n| n.even? }
   end
 
-  def test_foo
-    assert(kitty.name == 'Milo')
+  def complement_v1(predicate, value)
+    not predicate.call(value)
+  end
+
+  def complement_v2(predicate)
+    lambda do |value|
+      not predicate.call(value)
+    end
+  end
+
+  def test_v1
+    expected = [false, true]
+    result = [complement_v1(is_even, 4), complement_v1(is_even, 5)]
+    assert_equal(expected, result)
+  end
+
+  def test_v2
+    x = complement_v2(is_even)
+    expected = [false, true]
+    result = [x.call(4), x.call(5)]
+    assert_equal(expected, result)
   end
 end
