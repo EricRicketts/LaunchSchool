e9c834e108ad36e4a9c7e4ce1799f570ab6da929
EricRicketts <eric_ricketts@me.com>
2018-12-21 06:04:58 -0500

    Exercises More Topics Medium 1 exercise 6

--- anki/anki.rb
+++ anki/anki.rb
@@ -1,52 +1,22 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+# require 'minitest/autorun'
+# require 'minitest/pride'
 require 'pry-byebug'
 
+def compare(str, flag)
+  puts "Before: #{str}"
+  puts "After: #{yield(str, flag)}"
+end
 
-class Anki < Minitest::Test
-  attr_accessor :arr
-
-  def my_each(collection, &block)
-    size = collection.size
-    index = 0
-    while index < size
-      yield(collection[index])
-      index += 1
-    end
-    collection
+compare("foo", :upcase) do |str, flag|
+  case flag
+  when :upcase, :capitalize then str.send(flag)
+  else str
   end
+end
 
-  def my_select_v1(collection)
-    size = collection.size
-    result = []
-    index = 0
-    while index < size
-      result << collection[index] if yield(collection[index])
-      index += 1
-    end
-    result
+compare("foo", :capitalize) do |str, flag|
+  case flag
+  when :upcase, :capitalize then str.send(flag)
+  else str
   end
-
-  def my_select_v2(collection, &block)
-    result = []
-    my_each(collection) do |n|
-      result << n if block.call(n)
-    end
-    result
-  end
-
-  def setup
-    @arr = (1..10).to_a
-  end
-
-  def test_select_variants
-    result1 = my_select_v1(arr) { |n| n.odd? }
-    result2 = my_select_v2(arr) { |n| n.odd? }
-    expected = [
-      [1, 3, 5, 7, 9],
-      [1, 3, 5, 7, 9]
-    ]
-    assert_equal(expected, [result1, result2])
-  end
-
 end
--- exercises/more_topics/medium1/exercise_6_test.rb
+++ exercises/more_topics/medium1/exercise_6_test.rb
@@ -3,19 +3,37 @@ require 'minitest/pride'
 require 'pry-byebug'
 
 class Exercise6Test < Minitest::Test
+  attr_accessor :input, :expected
+
+  def setup
+    @input = [8, 10, 12, 14, 16, 33]
+    @expected = [10, 12, 14, 16, 20, 41]
+  end
 
   def convert_to_base_8(n)
-    arr_of_digits = Array.new
-    quotient, remainder = n.divmod(8)
-    if quotient > 8
-      convert_to_base_8(quotient).concat(remainder.to_s)
+    n.to_s(8).to_i
+  end
+
+  def convert_to_base(args)
+    base, n = args
+    quotient, remainder = n.divmod(base)
+    if quotient > base
+      convert_to_base([base, quotient]).concat(remainder.to_s)
     else
       "#{quotient.to_s + remainder.to_s}"
     end
   end
 
-  def test_base_8
-    foo = convert_to_base_8(1000)
-    binding.pry
+  def test_ls_base_8
+    base_8_proc = method(:convert_to_base_8).to_proc
+    result = input.map(&base_8_proc)
+    assert_equal(expected, result)
+  end
+
+  def test_my_convert_to_base
+    base_8_proc = method(:convert_to_base).to_proc
+    new_input = Array.new(6, 8).zip(input)
+    result = new_input.map(&base_8_proc).map(&:to_i)
+    assert_equal(expected, result)
   end
 end
