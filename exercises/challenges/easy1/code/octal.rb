8bc90c50587bd464a664897748fdb1663f1bfb3b
EricRicketts <eric_ricketts@me.com>
2019-01-01 05:27:40 -0500

    update Challenges Easy 1 exercise 3

--- exercises/challenges/easy1/code/octal.rb
+++ exercises/challenges/easy1/code/octal.rb
@@ -1,12 +1,13 @@
 class Octal
   BASE_EIGHT = 8
+  LARGEST_DIGIT = 7
 
   def initialize(digit_str)
     @base_eight_digits = convert_to_digit_array(digit_str)
   end
 
   def to_decimal
-    return 0 if base_eight_digits.any? { |digit| digit > 7 }
+    return 0 if base_eight_digits.any? { |digit| digit > LARGEST_DIGIT }
     base_eight_digits.each.with_index.inject(0) do |base_ten, (digit, exponent)|
       base_ten += digit*(BASE_EIGHT**exponent)
     end
