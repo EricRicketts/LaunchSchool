d9a3f27ce31e7927b05197b70614f47b14afbe2e
EricRicketts <eric_ricketts@me.com>
2019-01-02 05:55:43 -0500

    Challenges Easy 1 exercise 9

---
+++ exercises/challenges/easy1/exercise_9_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'pry-byebug'
+require_relative './code/perfect_numbers'
+Minitest::Reporters.use!
+
+class PerfectNumberTest < Minitest::Test
+  def test_initialize_perfect_number
+    # skip
+    assert_raises RuntimeError do
+      PerfectNumber.classify(-1)
+    end
+  end
+
+  def test_classify_deficient
+    # skip
+    assert_equal 'deficient', PerfectNumber.classify(13)
+  end
+
+  def test_classify_perfect
+    # skip
+    assert_equal 'perfect', PerfectNumber.classify(28)
+  end
+
+  def test_classify_abundant
+    # skip
+    assert_equal 'abundant', PerfectNumber.classify(12)
+  end
+end
