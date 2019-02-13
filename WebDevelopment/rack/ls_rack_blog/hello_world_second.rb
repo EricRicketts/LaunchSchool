e49aa65e788ca044fadaf4f828bcdd5e6174bb4b
EricRicketts <eric_ricketts@me.com>
2019-02-13 13:11:18 -0500

    refine second Launch School exercise for Rack

--- WebDevelopment/rack/ls_rack_blog/hello_world_second.rb
+++ WebDevelopment/rack/ls_rack_blog/hello_world_second.rb
@@ -8,17 +8,18 @@ class HelloWorldSecond
     when '/'
       ['200', CONTENT_TYPE, [generate_html]]
     when '/advice'
-      ['200', CONTENT_TYPE, [generate_html(Advice.new.generate)]]
+      ['200', CONTENT_TYPE, [generate_html(Advice.new.generate, ["b", "em"])]]
     else
-      ['404', { 'Content-Type' => 'text/html', 'Content-Length' => '209' },
-        [generate_html('404 Not Found')]
+      ['404', { 'Content-Type' => 'text/html', 'Content-Length' => '211' },
+        [generate_html('404 Not Found', ["h4"])]
       ]
     end
   end
 
   private
 
-  def generate_html(str="Hello World!")
+  def generate_html(str="Hello World!", elements=["h2"])
+    fragment = generate_html_fragment(str, elements)
     html = <<~HTML
     <!DOCTYPE html>
     <html>
@@ -27,10 +28,25 @@ class HelloWorldSecond
         <meta charset="utf-8">
       </head>
       <body>
-        <h3>Body of Second Rack Application</h3>
-        <p>#{str}</p>
+        <h1>Body of Second Rack Application</h1>
+        #{fragment}
       </body>
     </html>
     HTML
+  end
+
+  def generate_html_fragment(str, elements)
+    fragment = ''
+    elements.each do |elem|
+      fragment << '<' << elem << '>'
+    end
+
+    fragment << str
+
+    elements.reverse.each do |elem|
+      fragment << '</' << elem << '>'
+    end
+
+    fragment
   end
 end
