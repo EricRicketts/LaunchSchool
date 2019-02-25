5e0040dea5b1283d85530f319a945b5604043168
EricRicketts <eric_ricketts@me.com>
2019-02-25 16:53:11 -0500

    chpater 1 content

--- WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
+++ WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
@@ -10,3 +10,16 @@ get "/" do
     :toc => table_of_contents
   }
 end
+
+get "/chapters/1" do
+  table_of_contents = File.readlines('data/toc.txt', chomp: true)
+  chapter_1 = File.readlines('data/chp1.txt')
+  chapter_1_modified = chapter_1.map do |line|
+    line.gsub(/\n/, '<br>')
+  end
+  erb :chapter, :locals => {
+    :title => "The Adventures of Sherlock Holmes",
+    :toc => table_of_contents,
+    :chapter => chapter_1_modified
+  }
+end
--- exercises/challenges/medium1/code/secret_handshake.rb
+++ exercises/challenges/medium1/code/secret_handshake.rb
@@ -8,6 +8,26 @@ class SecretHandshake
   private
 
   def convert_to_binary(n)
-    n.to_s.match?(/[[:digit:]]+/) ? n.to_s(2)
+    case n
+    when Integer
+      n.to_s(2)
+    when /[01]+/
+      n
+    else
+      []
+    end
   end
 end
+
+=begin
+1.  Check if the input is an Integer, if so convert to binary
+2.  Next check if the input is a string and if the string only
+    consists of 1's and 0's if so process it, if not return
+    an empty array
+3.  take the binary numbers and reverse them
+4.  cycle through each binary number with index
+    index 0 digit 1 'wink'
+    index 1 digit 1 'double blink'
+    index 2 digit 1 'close your eyes'
+    index 3 digit 1 'jump'
+=end
