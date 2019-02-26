935353528210a537b9720a63ad8bba9203bf3b19
EricRicketts <eric_ricketts@me.com>
2019-02-26 14:06:13 -0500

    RB170 Working With Sinatra View Helpers

--- WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
+++ WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
@@ -3,28 +3,35 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require 'pry-byebug'
 
-get "/" do
-  table_of_contents = File.readlines('data/toc.txt', chomp: true)
-  chapter_titles = table_of_contents.map.with_index(1) do |title, idx|
-    "Chapter #{idx} #{title}"
+before do
+  @title = "The Adventures of Sherlock Holmes"
+  @table_of_contents = File.readlines('data/toc.txt', chomp: true)
+  @chapter_titles = @table_of_contents.map.with_index(1) do |title, idx|
+    "Chapter #{idx}:&nbsp;&nbsp;#{title}"
   end
+end
+
+helpers do
+  def in_paragraphs(chapter)
+    lines = chapter.split(/\n{2}/).map { |line| line.gsub(/\n/, ' ') }
+    paragraphs = lines.map { |line| "<p>" << line << "</p>" }
+    paragraphs.join
+  end
+end
+
+get "/" do
   erb :home, :locals => {
-    :title => "The Adventures of Sherlock Holmes",
-    :contents => table_of_contents
+    :title => @title,
+    :contents => @table_of_contents
   }
 end
 
 get "/chapters/:number" do
-  table_of_contents = File.readlines('data/toc.txt', chomp: true)
   chapter_number = params[:number].to_i
-  chapter = File.readlines("data/chp#{chapter_number}.txt")
-  chapter_titles = table_of_contents.map.with_index(1) do |title, idx|
-    "Chapter #{idx}:&nbsp;&nbsp;#{title}"
-  end
+  chapter = File.read("data/chp#{chapter_number}.txt")
   erb :chapter, :locals => {
-    :title => "The Adventures of Sherlock Holmes",
-    :contents => table_of_contents,
-    :chapter_title => chapter_titles[chapter_number - 1],
-    :chapter => chapter
+    :contents => @table_of_contents,
+    :chapter_title => @chapter_titles[chapter_number - 1],
+    :chapter => in_paragraphs(chapter)
   }
 end
