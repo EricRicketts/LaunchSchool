308bbdd899d81b2ebda4060d7128bf7dbf4368dd
EricRicketts <eric_ricketts@me.com>
2019-02-27 14:10:24 -0500

    have the search working but need to refactor

--- WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
+++ WebDevelopment/working_with_sinatra/book_viewer/book_viewer.rb
@@ -9,6 +9,10 @@ before do
   @chapter_titles = @table_of_contents.map.with_index(1) do |title, idx|
     "Chapter #{idx}:&nbsp;&nbsp;#{title}"
   end
+  @chapters = Dir.entries('data').select { |f| f.match?(/chp\d+\.txt/) }.sort_by do |chapter|
+    chapter.match(/\d+/)[0].to_i
+  end
+  @titles_and_chapters = (@table_of_contents.zip(@chapters)).to_h
 end
 
 helpers do
@@ -31,9 +35,32 @@ get "/chapters/:number" do
   redirect to("/"), 303 unless (1..@table_of_contents.size).cover?(chapter_number)
   chapter = File.read("data/chp#{chapter_number}.txt")
   erb :chapter, :locals => {
+    :title => @title,
     :contents => @table_of_contents,
     :chapter_title => @chapter_titles[chapter_number - 1],
     :chapter => in_paragraphs(chapter)
+  }
+end
+
+get "/search" do
+  search_term = params[:query]
+  @search_results = []
+  unless !search_term || search_term.strip.empty?
+    regex = Regexp.new(search_term)
+    @search_results = @titles_and_chapters.select do |title, chapter|
+      file = File.read("data/#{chapter}")
+      file.split(/\n/).find { |line| line.match?(regex) }
+    end
+  end
+  if @search_results.empty?
+    @results = []
+  else
+    @results = @search_results.map { |title, chapter| [title, chapter.scan(/\d+/)[0]] }.to_h
+  end
+  erb :search, :locals => {
+    :title => @title,
+    :contents => @table_of_contents,
+    :results => @results
   }
 end
 
