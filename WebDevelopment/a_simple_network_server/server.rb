9886069db1c36dc758068a899adcd09a8381fdcb
EricRicketts <eric_ricketts@me.com>
2019-03-16 06:51:25 -0400

    first iteration Launch School Exercises Challenges Medium 2 Exercise 1

--- WebDevelopment/a_simple_network_server/server.rb
+++ WebDevelopment/a_simple_network_server/server.rb
@@ -4,62 +4,17 @@ require 'pry-byebug'
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
-
-  request_line = client.gets
-  puts request_line
-
+  request_lines = []
+  single_line = ''
+  loop do
+    single_line = client.gets
+    break request_lines.push(single_line) if single_line.match?(/(\A|^)\r\n(\z|$)/)
+    request_lines.push(single_line)
+  end
   client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/plain\r\n\r\n"
-  client.puts request_line
+  request_lines.each do |line|
+    client.puts line
+  end
   client.close
 end
-
-=begin
-Just really wnat to understand what is going on here.  I used
-to be a hardware engineer before coming to Launch School, so maybe
-I try to dig into the details too much.
-
-Could anyone comment on my assumptions and answer my questions?
-
-So in line 4 we createa a TCP Server socket bound to "localhost"
-port 3003.  As the video stated, the internals of TCP/IP is
-not the focus in RB170.  However, based on my prior teaching
-in RB170, servers wait for requests and then respond to requests.
-In view of how servers work, then we have created our own
-server at IP Address 127.0.0.1:3003.
-
-Here is a question, is DNS still at work here, considering
-localhost is my own computer?  If I understand from our prior
-instruction, the ISP will take the domain name and send it out
-to the DNS servers, the DNS servers will return the IP Address
-to the ISP so that the browsers GET request is submitted to an
-actual IP address.
-
-On line 6, the video stated a wait state is entered until
-someone tries to request something from the server.  I say
-"wait state" because when I noticed the code does not advance
-until I actually hit return in the URL field of the browser.
-
-Once a request is made, then this server will accept the request and
-then it will open a connection to the client.  In the video
-it says the code returns a "client object", which is actually
-a TCPSocket object according to the documentation.  The video
-says this "client object" enables us to interact with whatever
-the remote system is.  If the server has to open a connection
-with the client then how did the server receive the request in
-the first place?  Some channel of communication had to be open.
-
-On line 8, we are using #gets to grab just the first line of
-the request.  This is where I get a bit confused.  So the client
-formulated the request and sent it out, so client.gets grabs
-the first line of the request.  Where is the text of the request
-stored so #gets can retrieve it?
-
-Are lines 11-13 "faking" a response from the server?  I can
-see we have the required status field for a response, a response
-header, and finally a response message body.  If this is true, how
-does client.puts go about simulating a response from the server?
-How does the browser know to display the "request_line" to the browser
-window?
-
-=end
---
+++ exercises/challenges/medium2/code/palindrome_products.rb
@@ -0,0 +1,44 @@
+class Hash
+  def factors
+    self.values.first
+  end
+
+  def value
+    self.keys.first
+  end
+end
+
+class Palindromes
+
+  def initialize(max_factor: 1, min_factor: 1)
+    @max_factor = max_factor
+    @min_factor = min_factor
+  end
+
+  def generate
+    self.palindromes = (min_factor..max_factor).to_a.repeated_permutation(2).select do |n1, n2|
+      (n1*n2).digits == (n1*n2).digits.reverse
+    end.sort_by { |n1, n2| n1*n2 }.map { |n1, n2| { n1*n2 => [n1, n2].sort } }.uniq
+  end
+
+  def largest
+    max_value = palindromes.last.keys.first
+    { max_value => generate_values(max_value) }
+  end
+
+  def smallest
+    min_value = palindromes.first.keys.first
+    { min_value => generate_values(min_value) }
+  end
+
+  private
+
+  attr_reader :min_factor, :max_factor
+  attr_accessor :palindromes
+
+  def generate_values(value)
+    palindromes.select { |h| h.keys.first == value }.map do |h|
+      h.values.first
+    end
+  end
+end
