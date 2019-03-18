181aebb3de6d47af9842ffdbb2745c24ba48ca10
EricRicketts <eric_ricketts@me.com>
2019-03-18 15:14:12 -0400

    update echo and server programs

--- WebDevelopment/a_simple_network_server/server.rb
+++ WebDevelopment/a_simple_network_server/server.rb
@@ -6,16 +6,19 @@ loop do
   client = server.accept
   request_lines = []
   single_line = ''
+
   loop do
     single_line = client.gets.chomp
     break if single_line.empty? || single_line.nil?
     request_lines.push(single_line)
     puts single_line
   end
+
   client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/plain\r\n\r\n"
   request_lines.each do |line|
     client.puts line
   end
+
   client.close
 end
