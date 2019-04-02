e2289f06c7f7432b4f6367639eceeda620d8568a
EricRicketts <eric_ricketts@me.com>
2019-04-02 04:29:19 -0400

    classic application working and refactored properly

--- WebDevelopment/working_with_sinatra/sinatra_todos/todo.rb
+++ WebDevelopment/working_with_sinatra/sinatra_todos/todo.rb
@@ -55,28 +55,28 @@ get '/lists/new' do
   erb :new_list, locals: { key: :none }, layout: :layout
 end
 
-get '/lists/:id' do |id|
-  list = session[:lists][id.to_i]
+get '/lists/:list_id' do |list_id|
+  list = session[:lists][list_id.to_i]
   key = session.key?(:success) ? :success : :none
-  erb :list, locals: { list: list, id: id, key: key }, layout: :layout
+  erb :list, locals: { list: list, list_id: list_id, key: key }, layout: :layout
 end
 
-get '/lists/:id/edit' do |id|
-  list = session[:lists][id.to_i]
-  erb :edit_list, locals: { list: list, id: id, key: :none }, layout: :layout
+get '/lists/:list_id/edit' do |list_id|
+  list = session[:lists][list_id.to_i]
+  erb :edit_list, locals: { list: list, list_id: list_id, key: :none }, layout: :layout
 end
 
-delete '/lists/:id' do |id|
-  session[:lists].delete_at(id.to_i)
+delete '/lists/:list_id' do |list_id|
+  session[:lists].delete_at(list_id.to_i)
   session[:success] = 'The list has been deleted.'
   redirect '/lists'
 end
 
-delete '/lists/:id/todos/:todo_id' do |id, todo_id|
-  list = session[:lists][id.to_i]
+delete '/lists/:list_id/todos/:todo_id' do |list_id, todo_id|
+  list = session[:lists][list_id.to_i]
   list[:todos].delete_at(todo_id.to_i)
   session[:success] = 'The todo has been deleted.'
-  redirect "/lists/#{id}"
+  redirect "/lists/#{list_id}"
 end
 
 post '/lists' do
@@ -93,34 +93,34 @@ post '/lists' do
   end
 end
 
-post '/lists/:id' do |id|
-  list = session[:lists][id.to_i]
+post '/lists/:list_id' do |list_id|
+  list = session[:lists][list_id.to_i]
   list_name = params[:list_name].strip
   error = error_for_list_name(list_name)
 
   if error
     set_flash(:error, error)
-    erb :edit_list, locals: { list: list, id: id, key: :error }, layout: :layout
+    erb :edit_list, locals: { list: list, list_id: list_id, key: :error }, layout: :layout
   else
     message = 'The list has been updated.'
     set_flash(:success, message)
-    session[:lists][id.to_i][:name] = list_name
-    redirect "/lists/#{id}"
+    session[:lists][list_id.to_i][:name] = list_name
+    redirect "/lists/#{list_id}"
   end
 end
 
-post '/lists/:id/todos' do |id|
-  list = session[:lists][id.to_i]
+post '/lists/:list_id/todos' do |list_id|
+  list = session[:lists][list_id.to_i]
   todo = params[:todo].strip
   error = error_for_todo(todo)
 
   if error
     session[:error] = error
-    erb :list, locals: { list: list, id: id, key: :error}, layout: :layout
+    erb :list, locals: { list: list, list_id: list_id, key: :error}, layout: :layout
   else
-    session[:lists][id.to_i][:todos] << { name: todo, completed: false }
+    session[:lists][list_id.to_i][:todos] << { name: todo, completed: false }
     message = 'The todo was added.'
     set_flash(:success, message)
-    redirect "/lists/#{id}"
+    redirect "/lists/#{list_id}"
   end
 end
