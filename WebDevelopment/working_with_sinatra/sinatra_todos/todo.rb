00e4ff1f6e04e0e9c37e68a152eb96202f0330c7
EricRicketts <eric_ricketts@me.com>
2019-04-09 17:06:08 -0400

    add list id property to classic application

--- WebDevelopment/working_with_sinatra/sinatra_todos/todo.rb
+++ WebDevelopment/working_with_sinatra/sinatra_todos/todo.rb
@@ -19,6 +19,11 @@ before do
 end
 
 helpers do
+  def create_list_id(lists)
+    max = lists.map { |list| list[:id] }.max || 0
+    max + 1
+  end
+
   def create_todo_id(todos)
     max = todos.map { |todo| todo[:id] }.max || 0
     max + 1
@@ -68,13 +73,17 @@ helpers do
     "#{todos_remaining(list)}/#{list_count(list)}"
   end
 
-  def load_list(index)
-    list = session[:lists][index] if index && session[:lists][index]
-    return list if list
-
-    message = 'The specified list was not found.'
-    set_flash(:error, message)
-    redirect '/lists'
+  def load_list(list_id)
+    # list = session[:lists][index] if index && session[:lists][index]
+    # return list if list
+    list = session[:lists].find { |list| list[:id] == list_id.to_i }
+    if list_id && list
+      list
+    else
+      message = 'The specified list was not found.'
+      set_flash(:error, message)
+      redirect '/lists'
+    end
   end
 
   def set_flash(key, message = '')
@@ -84,8 +93,11 @@ helpers do
   def sort_lists(lists, &block)
     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
 
-    incomplete_lists.each { |list| yield list, lists.index(list) }
-    complete_lists.each { |list| yield list, lists.index(list) }
+    incomplete_lists.sort_by! { |list| list[:id] }
+    complete_lists.sort_by! { |list| list[:id] }
+
+    incomplete_lists.each { |list| yield list }
+    complete_lists.each { |list| yield list }
   end
 
   def sort_todos(todos, &block)
@@ -118,18 +130,19 @@ get '/lists/new' do
 end
 
 get '/lists/:list_id' do |list_id|
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   key = session.key?(:success) ? :success : :none
   erb :list, locals: { list: list, list_id: list_id, key: key }, layout: :layout
 end
 
 get '/lists/:list_id/edit' do |list_id|
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   erb :edit_list, locals: { list: list, list_id: list_id, key: :none }, layout: :layout
 end
 
 delete '/lists/:list_id' do |list_id|
-  session[:lists].delete_at(list_id.to_i)
+  idx = session[:lists].index { |list| list[:id] == list_id.to_i }
+  session[:lists].delete_at(idx)
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
   else
@@ -139,7 +152,7 @@ delete '/lists/:list_id' do |list_id|
 end
 
 delete '/lists/:list_id/todos/:todo_id' do |list_id, todo_id|
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   idx = list[:todos].index { |todo| todo[:id] == todo_id.to_i }
   list[:todos].delete_at(idx)
 
@@ -153,14 +166,14 @@ end
 
 patch '/lists/:list_id/todos/:todo_id' do |list_id, todo_id|
   completed_value = params[:completed].to_s == "true"
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   list[:todos].find { |todo| todo[:id] == todo_id.to_i }[:completed] = completed_value
   session[:success] = "The todo has been updated."
   redirect "/lists/#{list_id}"
 end
 
 patch '/lists/:list_id/todos' do |list_id|
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   list[:todos].each { |todo| todo[:completed] = true }
   session[:success] = "All todos have been completed."
   redirect "/lists/#{list_id}"
@@ -169,20 +182,22 @@ end
 post '/lists' do
   list_name = params[:list_name].strip
   error = error_for_list_name(list_name)
+
   if error
     set_flash(:error, error)
     erb :new_list, locals: { key: :error }, layout: :layout
   else
     message = 'The list has been created.'
     set_flash(:success, message)
-    session[:lists] << { name: list_name, todos: [] }
+    id = create_list_id(session[:lists])
+    session[:lists] << { id: id, name: list_name, todos: [] }
     redirect '/lists'
   end
 end
 
 post '/lists/:list_id' do |list_id|
   list_name = params[:list_name].strip
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   error = error_for_list_name(list_name)
 
   if error
@@ -191,22 +206,23 @@ post '/lists/:list_id' do |list_id|
   else
     message = 'The list has been updated.'
     set_flash(:success, message)
-    session[:lists][list_id.to_i][:name] = list_name
+    list[:name] = list_name
     redirect "/lists/#{list_id}"
   end
 end
 
 post '/lists/:list_id/todos' do |list_id|
   todo = params[:todo].strip
-  list = load_list(list_id.to_i)
+  list = load_list(list_id)
   error = error_for_todo(todo)
 
   if error
     session[:error] = error
     erb :list, locals: { list: list, list_id: list_id, key: :error}, layout: :layout
   else
-    todo_properites = { id: create_todo_id(list[:todos]), name: todo, completed: false }
-    session[:lists][list_id.to_i][:todos] << todo_properites
+    id = create_todo_id(list[:todos])
+    todo_properties = { id: id, name: todo, completed: false }
+    list[:todos] << todo_properties
     message = 'The todo was added.'
     set_flash(:success, message)
     redirect "/lists/#{list_id}"
