baf2e3a345af979657a2a508430e00f2dccff0a9
EricRicketts <eric_ricketts@me.com>
2019-04-13 13:42:12 -0400

    add links to edit the files

--- WebDevelopment/working_with_sinatra/file_based_cms/test/cms_rack_test.rb
+++ WebDevelopment/working_with_sinatra/file_based_cms/test/cms_rack_test.rb
@@ -7,7 +7,7 @@ require 'pry-byebug'
 require_relative '../cms'
 
 class CmsRackTest < Minitest::Test
-  attr_reader :home_page, :dir, :fname
+  attr_reader :home_page, :dir, :fnames
 
   include Rack::Test::Methods
 
@@ -17,17 +17,26 @@ class CmsRackTest < Minitest::Test
 
   def setup
     @home_page = '/'
+    @fnames = %w[foo.txt foo.md]
     path = File.expand_path(__FILE__)
-    @dir = File.dirname(path).sub(/\/test/, "/data")
-    @fname = '/foo.md'
-    File.open(dir + fname, "w+") do |f|
-      f.puts '# Foo Title'
-      f.puts 'This is a paragraph in foo.md.'
+    @dir = File.dirname(path).sub(/\/test/, "/data/")
+    fnames.each do |fname|
+      File.open(dir + fname, "w+") do |f|
+        if fname == 'foo.txt'
+          f.puts 'First line of foo.txt'
+          f.puts 'This is the second line in foo.txt which is a text file.'
+        else
+          f.puts '# Foo'
+          f.puts 'This is a paragraph in foo.md which is a markdown file.'
+        end
+      end
     end
   end
 
   def teardown
-    File.delete(dir + fname) if File.exists?(dir + fname)
+    fnames.each do |fname|
+      File.delete(dir + fname) if File.exists?(dir + fname)
+    end
   end
 
   def test_home_page
@@ -36,22 +45,22 @@ class CmsRackTest < Minitest::Test
     assert_equal(200, last_response.status)
     assert_equal('text/html;charset=utf-8', last_response.headers['Content-Type'])
     links = last_response.body.scan(/<a href="\/[\w\.]+">[\w\.]+<\/a>/)
-    assert_equal(4, links.size)
+    assert_equal(5, links.size)
   end
 
-  def test_home_page_links
-    skip
-    %w[changes.txt history.txt].each do |fname|
-      url = home_page + fname
-      get url
-      assert_equal(200, last_response.status)
-      assert_equal('text/plain', last_response.headers['Content-Type'])
-      assert_equal("This is the #{fname} file.", last_response.body)
-    end
+  def test_text_file_content
+    # skip
+    url = home_page + fnames.first
+    get url
+    assert_equal(200, last_response.status)
+    assert_equal('text/plain', last_response.headers['Content-Type'])
+    expected = "First line of foo.txt\n" +
+    "This is the second line in foo.txt which is a text file.\n"
+    assert_equal(expected, last_response.body)
   end
 
   def test_invalid_route
-    skip
+    # skip
     filename = 'bar.txt'
     url = home_page + filename
     expected = "#{filename} does not exist."
@@ -68,10 +77,10 @@ class CmsRackTest < Minitest::Test
   end
 
   def test_markdown_renders_to_html
-    skip
-    url = home_page << 'about.md'
-    header = '<h1>Ruby</h1>'
-    paragraph = '<p>An elegant programming language.</p>'
+    # skip
+    url = home_page + fnames.last
+    header = '<h1>Foo</h1>'
+    paragraph = '<p>This is a paragraph in foo.md which is a markdown file.</p>'
     get url
 
     assert_equal(200, last_response.status)
@@ -80,8 +89,18 @@ class CmsRackTest < Minitest::Test
     assert_includes(last_response.body, paragraph)
   end
 
-  def test_edit_a_file
-    skip
-    url = home_page
+  def test_edit_links_exist
+    # skip
+    url = home_page + fnames.first
+    get home_page
+
+    edit_links = [
+      "<a href=\"/#{fnames.first}/edit\">Edit</a>",
+      "<a href=\"/#{fnames.last}/edit\">Edit</a>"
+    ]
+
+    edit_links.each do |edit_link|
+      assert_includes(last_response.body, edit_link)
+    end
   end
 end
