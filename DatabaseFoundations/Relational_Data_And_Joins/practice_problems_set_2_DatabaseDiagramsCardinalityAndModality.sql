c00caffa49e8eb9ecf2362ddeaabcf3062d96349
EricRicketts <eric_ricketts@me.com>
2019-07-08 14:18:31 -0400

    Relational Data and JOINS; practice problems; set 2; Database Diagrams Cardinality and Modality

---
+++ DatabaseFoundations/Relational_Data_And_Joins/practice_problems_set_2_DatabaseDiagramsCardinalityAndModality.sql
@@ -0,0 +1,21 @@
+/*
+Exercise 1
+
+What is Cardinality?
+It is the number of objects on each side of the relationship (1:1, 1:M, M:M).
+
+Exercise 2
+
+What is Modality?
+if the reltionship is required (1, meaning there must be at least one instance of the entity) or optional (0, meaning no instances of the entity is required).
+
+Exercise 3
+
+If one side of a relationship has a modality of 1, what is the smallest number of instances that can be on that side of the relationship?
+One.
+
+Exericse 4
+
+What type of notation was described in depth throughout most of the video?
+Crow's Foot.
+*/
