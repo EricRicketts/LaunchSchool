6369376e627215b3d1518dc240d867b6b8d0c1aa
EricRicketts <eric_ricketts@me.com>
2019-07-23 20:07:02 -0400

    project setup for command line project using PG gem

--- exercises/challenges/medium2/code/run_length_encoding.rb
+++ exercises/challenges/medium2/code/run_length_encoding.rb
@@ -8,3 +8,20 @@ class RunLengthEncoding
     input.gsub(/(\d*)(.)/) { $1.empty? ? $2 : $2 * $1.to_i }
   end
 end
+
+=begin
+
+module RunLengthEncoding
+  def self.encode(str)
+    str.gsub(/(.)\1+/) { |match| "#{match.size}#{match[0]}" }
+  end
+
+  def self.decode(str)
+    str.gsub(/(\d+)(.)/) { $2 * $1.to_i }
+  end
+end
+
+similar to mine but cleaner.  First, the module was a better design
+choice as a class makes more sense if it has instance methods.  Secondly,
+he made use of the fact 
+=end
