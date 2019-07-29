797c0fcdc6ab6cc394c3c997c18bcfd00ea30071
EricRicketts <eric_ricketts@me.com>
2019-07-29 09:20:02 -0400

    completed project with conditional schema code

--- DatabaseApplications/interacting_with_a_database_in_code/command_line_project/insert_expenses.sql
+++ DatabaseApplications/interacting_with_a_database_in_code/command_line_project/insert_expenses.sql
@@ -1,9 +1,9 @@
-INSERT INTO expenses (amount, memo) VALUES
-(14.56, 'Pencils'),
-(3.29, 'Coffee'),
-(49.99, 'Text Editor'),
-(30.00, 'YNAB Budget software'),
-(21.99, 'Melvin''s software'),
-(21.31, '3 dozen donuts'),
-(1.99, 'Notebook'),
-(35.67, 'Micro weight Olympic Plates');
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(14.56, 'Pencils', '2019-07-01 15:30:30-04'),
+(3.29, 'Coffee', '2019-07-02 16:42:1-04'),
+(49.99, 'Text Editor', '2019-07-03 08:10:10-04'),
+(30.00, 'YNAB Budget software', '2019-07-04 04:17:23-04'),
+(21.99, 'Melvin''s software', '2019-07-05 11:30:47-04'),
+(21.31, '3 dozen donuts', '2019-07-06 14:11:12-04'),
+(1.99, 'Notebook', '2019-07-07 20:20:20-04'),
+(35.67, 'Micro weight Olympic Plates', '2019-07-08 21:01:33-04');
