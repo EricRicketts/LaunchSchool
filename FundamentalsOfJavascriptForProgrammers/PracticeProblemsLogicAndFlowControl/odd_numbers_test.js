f80cdb4913f9b3b3d2341c0e65d55bcc8ec70ed2
EricRicketts <eric_ricketts@me.com>
2019-11-09 14:58:56 -0500

    Javascript Language Fundamentals; Practice Problems; Multiples Of 3 And 5

--- FundamentalsOfJavascriptForProgrammers/PracticeProblemsLogicAndFlowControl/odd_numbers_test.js
+++ FundamentalsOfJavascriptForProgrammers/PracticeProblemsLogicAndFlowControl/odd_numbers_test.js
@@ -1,11 +1,15 @@
 describe('Odd Numbers', function () {
   var expected;
 
+  function isEven (val) {
+    return (val % 2) === 0;
+  }
+
   function logOddNumbers (end) {
     var arr = [];
     for (var i = 1; i <= end; i++) {
       if ((i % 2) === 1) {
-        arr.push(i)
+        arr.push(i);
       }
     }
 
@@ -14,6 +18,28 @@ describe('Odd Numbers', function () {
 
   function logOddNumbersVersionTwo (end) {
     return [...Array(end + 1).keys()].slice(1).filter(n => (n % 2) === 1);
+  }
+
+  function logOddNumbersVersionThree (end) {
+    var arr = [];
+    for (var i = 1; i <= end; i += 2) {
+      arr.push(i);
+    }
+
+    return arr;
+  }
+
+  function logOddNumbersVersionFour (end) {
+    var arr = [];
+    for (var i = 0; i <= end; i++) {
+      if (isEven(i)) {
+        continue;
+      } else {
+        arr.push(i)
+      }
+    }
+
+    return arr;
   }
 
   beforeEach(function () {
@@ -29,4 +55,15 @@ describe('Odd Numbers', function () {
     expect(logOddNumbersVersionTwo(19)).to.deep.equal(expected);
     done();
   });
+
+  it('third version logs odd numbers', function (done) {
+    expect(logOddNumbersVersionThree(19)).to.deep.equal(expected);
+    done();
+  });
+
+  it('fourth version logs odd numbers', function (done) {
+    expect(logOddNumbersVersionFour(19)).to.deep.equal(expected);
+    done();
+  });
+
 });
