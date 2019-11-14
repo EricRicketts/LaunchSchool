2074953f44fdf647ccd768b1d253dd07e6585fe7
EricRicketts <eric_ricketts@me.com>
2019-11-14 13:19:07 -0500

    Fundamentals of Javascript For Programmers; Practice Problems Logic and Control Flow; Repeating Strings; separate test from code under test

--- FundamentalsOfJavascriptForProgrammers/PracticeProblemsLogicAndFlowControl/repeating_strings_test.js
+++ FundamentalsOfJavascriptForProgrammers/PracticeProblemsLogicAndFlowControl/repeating_strings_test.js
@@ -1,26 +1,4 @@
 describe('Repeating Strings', function () {
-  function multiplyString(str, n) {
-    var multipliedString = '';
-
-    for(var i = 0; i < n; i++) {
-      multipliedString += str;
-    }
-
-    return multipliedString;
-  }
-
-  function repeat(str, n) {
-    var repeatedString;
-
-    if ((typeof n !== 'number') || n < 0 || !Number.isFinite(n)) {
-      repeatedString = undefined;
-    } else {
-      repeatedString = multiplyString(str, n);
-    }
-
-    return repeatedString;
-  }
-
   it('repeat a string with a string is undefined', function (done) {
     expect(repeat('abc', 'a')).to.be.undefined;
     done();
