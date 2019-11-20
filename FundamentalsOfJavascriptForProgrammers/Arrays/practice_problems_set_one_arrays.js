1ec51aea99ec00d1e1d219158aa9605f5cecc439
EricRicketts <eric_ricketts@me.com>
2019-11-20 08:50:31 -0500

    JS210 Fundamentals of Javascript for Programmers, Arrays, Practice Problems, Set 1, Arrays; complete

--- FundamentalsOfJavascriptForProgrammers/Arrays/practice_problems_set_one_arrays.js
+++ FundamentalsOfJavascriptForProgrammers/Arrays/practice_problems_set_one_arrays.js
@@ -1,4 +1,51 @@
+function concatValues(arr) {
+  var str = '';
+  var lastIndex = arr.length - 1;
+
+  for(var i = 0; i <= lastIndex; i++) {
+    str += String(arr[i]);
+  }
+
+  return str;
+}
+
+function findValIn(arr, value) {
+  var indexValue = -1;
+  var lastIndex = arr.length - 1;
+
+  for(var i = 0; i <= lastIndex; i++) {
+    if (arr[i] === value) {
+      indexValue = i;
+      break;
+    }
+  }
+
+  return indexValue;
+}
+
 function lastInArray(arr) {
   var lastIndex = arr.length - 1;
   return arr[lastIndex];
+}
+
+function reverse(arr) {
+  var lastIndex = arr.length - 1;
+  var reversedArr = [];
+
+  for(var i = lastIndex; i >= 0; i--) {
+    reversedArr.push(arr[i]);
+  }
+
+  return reversedArr;
+}
+
+function rollCall(arr) {
+  var str = '';
+  var arrSize = arr.length;
+
+  for(var i = 0; i < arrSize; i++) {
+    str += arr[i] + '\n';
+  }
+
+  return str;
 }
--- FundamentalsOfJavascriptForProgrammers/Arrays/practice_problems_set_one_arrays_test.js
+++ FundamentalsOfJavascriptForProgrammers/Arrays/practice_problems_set_one_arrays_test.js
@@ -1,7 +1,35 @@
 describe('Practice Problems: Arrays', function () {
+  var arr, expected;
+
   it('Problem 1: Array Last Value', function (done) {
-    var arr = [0, 1, 2, 3, 4];
+    arr = [0, 1, 2, 3, 4];
     expect(lastInArray(arr)).to.equal(4);
     done();
+  });
+
+  it('Problem 2: Roll Call', function (done) {
+    arr = ['Foo', 'Bar', 'Fizz', 'Buzz'];
+    expect(rollCall(arr)).to.equal('Foo\nBar\nFizz\nBuzz\n');
+    done();
+  });
+
+  it('Problem 3: Reverse Values', function (done) {
+    arr = [1, 'foo', 10, 'bar'];
+    expected = ['bar', 10, 'foo', 1];
+    expect(reverse(arr)).to.deep.equal(expected); 
+    done();
+  });
+
+  it('Problem 4: Find a value', function (done) {
+    arr = ['in', 'out', 1, 3, 'fizzbuzz', 'up', 'down'];
+    expect(findValIn(arr, 'up')).to.equal(5);
+    expect(findValIn(arr, 'fizz')).to.equal(-1);
+    done();
+  });
+
+  it('Problem 5: Concat Values', function (done) {
+    arr = ['in', 1, true, undefined, null];
+    expect(concatValues(arr)).to.equal('in1trueundefinednull');
+    done();  
   });
 });
