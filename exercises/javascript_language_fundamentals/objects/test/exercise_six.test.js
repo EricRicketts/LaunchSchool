d8d004a0505273356859495647fcb037caa53852
EricRicketts <eric_ricketts@me.com>
2019-12-27 08:28:28 -0500

    Exercises: Javascript Language Fundamentals: Objects: Exercise Six

---
+++ exercises/javascript_language_fundamentals/objects/test/exercise_six.test.js
@@ -0,0 +1,30 @@
+describe('Exercises: Javascript Language Fundamentals: Objects: Exercise Six', () => {
+  var myArray, average;
+
+  beforeEach(() => {
+    myArray = [5, 5];
+    myArray[-1] = 5;
+    myArray[-2] = 5;
+
+    average = function(array) {
+      var sum = 0;
+      var i;
+
+      for (i = -2; i < array.length; i += 1) {
+        sum += array[i];
+      }
+
+      return sum / array.length;
+    }
+  });
+
+  it('Array length is only over indexed items', () => {
+    expect(average(myArray)).toBe(10);
+  });
+});
+/*
+the problem with this code is that the function iterates over the non-indexed
+properties and addes those property values to the sum.  However, the length
+property only gives the array length based on indexed properties, thus the
+developer might have expected to get 5 as the average but got 10 instead.
+*/
