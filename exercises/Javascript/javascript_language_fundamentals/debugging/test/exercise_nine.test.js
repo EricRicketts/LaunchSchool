2adf1b9f143a518ba67ae332adf5e0ecdeb8e298
EricRicketts <eric_ricketts@me.com>
2020-01-21 03:24:29 -0500

    Exercises JS210 Javascript Language Fundamentals Debugging Exercise Nine

---
+++ exercises/Javascript/javascript_language_fundamentals/debugging/test/exercise_nine.test.js
@@ -0,0 +1,14 @@
+import { updateCart, checkout } from "../code/expensive_study_preparation";
+
+describe('Exercises JS210 Javascript Language Fundamentals Debugging Exercise Nine', function () {
+  beforeEach(() => {
+    updateCart('notebook');
+    updateCart('pencil', 2);
+    updateCart('coffee', 1);
+    updateCart('pencil', 0);
+  });
+  it('charges too much for the items', function () {
+    // should be $12.99 as there are no pencils, a pencil was added in
+    expect(checkout()).toBe('You have been charged $14.69.');
+  });
+});
