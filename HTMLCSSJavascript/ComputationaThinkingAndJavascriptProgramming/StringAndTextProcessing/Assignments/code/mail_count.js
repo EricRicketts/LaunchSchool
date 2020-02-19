2c131bfcbc3946f863c85e284479e724d974efc0
EricRicketts <eric_ricketts@me.com>
2020-02-19 03:04:46 -0500

    LS15 String And Text Processing Practice Problem Mail Count

--- HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/StringAndTextProcessing/Assignments/code/mail_count.js
+++ HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/StringAndTextProcessing/Assignments/code/mail_count.js
@@ -1,8 +1,23 @@
-function mailCount(str) {
-/*
-emailData.split(/##\|\|##/).length;
-email.match(/(?<=From:).*(?=#\/#)/);
- */
+function mailCount(emailData) {
+  let results = {
+    emailCount: '',
+    dateRange: '',
+  };
+  let numberOfEmails = emailData.split(/##\|\|##/).length;
+  results.emailCount = 'Count of Email: ' + numberOfEmails;
+
+  let dateStrings = emailData.match(/(?<=Date:).*(?=#\/#)/g);
+  let dateObjects = dateStrings.map((dateString) => new Date(dateString) );
+  let sortedDates = dateObjects.sort(function(dateOne, dateTwo) {
+    return dateOne.valueOf() - dateTwo.valueOf();
+  });
+
+  let earliestDate = sortedDates[0];
+  let latestDate = sortedDates[sortedDates.length - 1];
+
+  results.dateRange = 'Date Range: ' + earliestDate.toDateString() + ' - ' + latestDate.toDateString();
+
+  return results;
 }
 
 export { mailCount };
