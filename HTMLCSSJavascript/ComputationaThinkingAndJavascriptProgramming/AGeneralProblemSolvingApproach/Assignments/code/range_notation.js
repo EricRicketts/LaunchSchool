35c05c513c883a07aab069f4cf61cbd6c4c3d149
EricRicketts <eric_ricketts@me.com>
2020-03-01 06:23:35 -0500

    begin redesign of Practice Problem Four LS215 A General Problem Solving Approach

--- HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/AGeneralProblemSolvingApproach/Assignments/code/range_notation.js
+++ HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/AGeneralProblemSolvingApproach/Assignments/code/range_notation.js
@@ -1,45 +1,33 @@
-function addToFinalSequence(priorNumber, currentNumber, finalSequence) {
-  if (priorNumber < currentNumber) {
-    finalSequence.push(currentNumber);
-  } else {
-    finalSequence.push(increaseNumberBySomePowerOfTen(priorNumber, currentNumber));
+function getNextSequenceNumber(currentNumber, priorNumber = 0) {
+  let newNumber = Number.parseInt(currentNumber, 10);
+
+  return priorNumber < newNumber ? newNumber : iterateForNextNumber(currentNumber, priorNumber);
+}
+
+function iterateForNextNumber(currentNumber, priorNumber) {
+  let newNumber = Number.parseInt(currentNumber, 10);
+  let originalCurrentNumber = Number.parseInt(currentNumber, 10);
+  let exponent = currentNumber.length;
+  let powerOfTen = Math.pow(10, exponent);
+  let numberFound;
+
+  while (true) {
+    newNumber += powerOfTen;
+    numberFound = (newNumber > priorNumber) && (newNumber % powerOfTen === originalCurrentNumber);
+    if (numberFound) { break; }
   }
+
+  return newNumber;
 }
 
-function increaseNumberBySomePowerOfTen(priorNumber, currentNumber) {
-  let exponent = Math.floor(Math.log10(currentNumber)) + 1;
-  let PowerOfTenIncrementer = Math.pow(10, exponent);
-
-  while (currentNumber < priorNumber) { currentNumber += PowerOfTenIncrementer; }
-  return currentNumber;
-}
-
-function processNumericSequence(arrOfNumericStrings) {
-  let numericArray = arrOfNumericStrings.map((numericString) => Number.parseInt(numericString, 10));
-  let finalSequence = [];
-  numericArray.forEach((currentNumber, idx) => {
-    let priorNumber = idx === 0 ? 0 : finalSequence[idx - 1];
-    addToFinalSequence(priorNumber, currentNumber, finalSequence);
-  });
-
-  return finalSequence;
-}
-
-function processRange(arrOfNumericStrings) {
-
+function range(start, end, length = end - start + 1) {
+  return Array.from({ length }, (_, i) => start + i);
 }
 
 function shortHandRangeToArray(rangeString) {
   const comma = /,\s*/;
   const rangeSeparators = /[:\-]|\.{2}/;
 
-  let parsedNumbersAndRanges = rangeString.split(comma).map((str) => str.split(rangeSeparators));
-
-  let arrOfNumericStrings = parsedNumbersAndRanges.map((arrOfNumericStrings) => {
-    return arrOfNumericStrings.length > 1 ? processRange(arrOfNumericStrings) : arrOfNumericStrings;
-  }).flat();
-
-  return processNumericSequence(arrOfNumericStrings)
 }
 
-export { shortHandRangeToArray };
+export { shortHandRangeToArray, range, getNextSequenceNumber };
--- HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/AGeneralProblemSolvingApproach/Assignments/test/practice_problem_four_range_notation.test.js
+++ HTMLCSSJavascript/ComputationaThinkingAndJavascriptProgramming/AGeneralProblemSolvingApproach/Assignments/test/practice_problem_four_range_notation.test.js
@@ -1,16 +1,96 @@
-import { shortHandRangeToArray } from "../code/range_notation";
+import { shortHandRangeToArray, range, getNextSequenceNumber } from "../code/range_notation";
 
 describe('LS215 A General Problem Solving Approach Practice Problem Four Range Notation', function () {
   let rangeString, expected;
-  it('should provide a simple range with comma separated numbers only', function () {
+  it('should return the bigger of two numbers for the next number in the sequence', function () {
+    expect(getNextSequenceNumber('9', 3)).toBe(9);
+  });
+
+  it('should iterate if current number is smaller than prior number', function () {
+    expect(getNextSequenceNumber('3', 8)).toBe(13);
+  });
+
+  it('should iterate if current number is equal to prior number', function () {
+    expect(getNextSequenceNumber('2', 2)).toBe(12);
+  });
+
+  it('should iterate with a prior number in the tens', function () {
+    expect(getNextSequenceNumber('2', 54)).toBe(62);
+  });
+
+  it('should iterate with a prior number in the hundreds', function () {
+    expect(getNextSequenceNumber('5', 345)).toBe(355);
+  });
+
+  it('should iterate with current number having a leading zero', function () {
+    expect(getNextSequenceNumber('02', 104)).toBe(202);
+  });
+
+  it('should iterate with prior number and current number in tens', function () {
+    expect(getNextSequenceNumber('15', 35)).toBe(115);
+  });
+
+  it('should iterate with a three digit current number', function () {
+    expect(getNextSequenceNumber('123', 421)).toBe(1123);
+  });
+
+  it.skip('should provide a simple range with comma separated numbers only', function () {
     rangeString = "1, 3, 7, 2, 4, 1";
     expected = [1, 3, 7, 12, 14, 21];
     expect(shortHandRangeToArray(rangeString)).toEqual(expected);
   });
 
-  it('should handle larger required power of ten increments', function () {
+  it.skip('should handle larger required power of ten increments', function () {
     rangeString = '5, 8, 45, 2, 101, 2, 443, 32, 732, 189';
     expected = [5, 8, 45, 52, 101, 102, 443, 532, 732, 1189];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should show an increasing sequence even if two numbers in shorthand are the same', function() {
+    rangeString = '5, 1, 2, 2, 1';
+    expected = [5, 11, 12, 22, 31];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should do a simple range', function () {
+    rangeString = '1-3';
+    expected = [1, 2, 3];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should do a range wit.skiph more than two endpoints', function () {
+    rangeString = '1:5:2';
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should process shorthand notation for three and one digit.skip pairing', function () {
+    rangeString = '104:2';
+    expected = [104, 105, 106, 107, 108, 109, 110, 111, 112];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should process shorthand notation for three and two digit.skip pairing', function () {
+    rangeString = '104:02';
+    expected = range(104, 202);
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should process two ranges in a row', function () {
+    rangeString = '1-3, 1-2';
+    expected = [1, 2, 3, 11, 12];
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should process a mix of single numbers and ranges', function () {
+    rangeString = '545, 64:11';
+    expected = [545].concat(range(564, 611));
+    expect(shortHandRangeToArray(rangeString)).toEqual(expected);
+  });
+
+  it.skip('should process alternating mix of single numbers and ranges', function () {
+    rangeString = '545, 64:11, 12, 13, 15-7';
+    expected = [545].concat(range(564, 611), 612, 613, range(615, 707));
     expect(shortHandRangeToArray(rangeString)).toEqual(expected);
   });
 });
