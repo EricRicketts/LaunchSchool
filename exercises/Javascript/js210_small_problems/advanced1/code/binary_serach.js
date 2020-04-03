03160a505260e587921121e9af2c833db0bec52e
EricRicketts <eric_ricketts@me.com>
2020-04-03 05:23:02 -0400

    JS210 Small Problems Advanced One Exercise Seven Manually Find The Index Of The Search Value

--- exercises/Javascript/js210_small_problems/advanced1/code/binary_serach.js
+++ exercises/Javascript/js210_small_problems/advanced1/code/binary_serach.js
@@ -2,7 +2,7 @@ function binarySearch(arr, searchValue, offset = 0) {
   let middleIndex = Math.floor(arr.length/2);
   let middleValue = arr[middleIndex];
   if (searchValue === middleValue) {
-    let foundIndex = arr.findIndex((el) => el === middleValue);
+    let foundIndex = findElementIndex(arr, searchValue);
     return offset + foundIndex;
   }  else if (searchValue !== middleValue && arr.length === 1) {
     return -1;
@@ -13,6 +13,14 @@ function binarySearch(arr, searchValue, offset = 0) {
       return binarySearch(arr.slice(0, middleIndex), searchValue, offset);
     }
   }
+}
+
+function findElementIndex(arr, value) {
+  let arrSize = arr.length;
+  let idx;
+
+  for (idx = 0; idx < arrSize; idx += 1) { if (value === arr[idx]) break; }
+  return idx;
 }
 
 export { binarySearch };
