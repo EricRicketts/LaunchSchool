640804242a0f12d0d3ab9765a215cc10d5e74cd8
EricRicketts <eric_ricketts@me.com>
2020-04-13 18:45:23 -0400

    JS210 Small Problems Interpretive Problem Solving Exercise Five

--- exercises/Javascript/js210_small_problems/interpretive_problem_solving/test/exercise_five.test.js
+++ exercises/Javascript/js210_small_problems/interpretive_problem_solving/test/exercise_five.test.js
@@ -11,4 +11,27 @@ describe('JS210 Small Problems Interpretive Problem Solving Exercise Five', func
       expected = 'Bmnxmtpeqw dhz\'x gh ar pbldal!';
       expect(vigenereEncrypt(phrase, textKey)).toBe(expected);
    });
+
+   it('should return the phrase with a character text key of "A"', function () {
+      textKey = 'A';
+      expect(vigenereEncrypt(phrase, textKey)).toBe(phrase);
+   });
+
+   it('should return phrase with a two character text key of "Aa"', function () {
+      textKey = 'Aa';
+      expect(vigenereEncrypt(phrase, textKey)).toBe(phrase);
+   });
+
+   it('should encode a phrase with a three character text key', function () {
+      textKey = 'cab';
+      expected = 'Riogaqrlfu dpp\'t hq oo riabat!';
+      expect(vigenereEncrypt(phrase, textKey)).toBe(expected);
+   });
+
+   it('should encode a phrase where text key is longer than phrase', function () {
+      phrase = 'Dog';
+      textKey = 'Rabbit';
+      expected = 'Uoh';
+      expect(vigenereEncrypt(phrase, textKey)).toBe(expected);
+   });
 });
