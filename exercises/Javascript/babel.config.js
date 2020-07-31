e5faa0fa409a4e1b5aa7de4241ed57a9da009246
EricRicketts <eric_ricketts@me.com>
2020-07-30 20:33:40 -0400

    configuration cleanup

--- exercises/Javascript/babel.config.js
+++ exercises/Javascript/babel.config.js
@@ -1,17 +1,13 @@
-const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
-      },
-      useBuiltIns: "usage",
-      corejs: 3
-    },
-  ],
-];
+module.exports = function(api) {
+  api.cache(true);
+  const presets = ["@babel/preset-env"];
+  const plugins = [
+    ["@babel/plugin-proposal-class-properties", {}],
+    ["@babel/plugin-transform-runtime", { "regenerator": true }]
+  ];
 
-module.exports = { presets };
+  return {
+    presets,
+    plugins
+  };
+}
