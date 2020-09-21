8cc287687bf56df514999b3f4296ebee24a30e87
EricRicketts <eric_ricketts@me.com>
2020-09-21 19:26:25 -0400

    Not quite finished with Part 4 of Search Autocomplete

--- HTMLCSSJavascript/DOMAndAsynchronousProgrammingWithJavascript/MakingHTTPRequestsFromJavascript/autocomplete_node/public/javascripts/autocomplete.js
+++ HTMLCSSJavascript/DOMAndAsynchronousProgrammingWithJavascript/MakingHTTPRequestsFromJavascript/autocomplete_node/public/javascripts/autocomplete.js
@@ -22,6 +22,51 @@ const Autocomplete = {
 
   bindEvents: function() {
     this.input.addEventListener('input', this.valueChanged.bind(this));
+    this.input.addEventListener('keydown', this.handleKeydown.bind(this));
+  },
+
+  handleKeydown: function(event) {
+    switch(event.key) {
+      case 'ArrowDown': {
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === this.matches.length - 1) {
+          this.selectedIndex = 0;
+        } else {
+          this.selectedIndex += 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      }
+      case 'ArrowUp': {
+        event.preventDefault();
+        if (this.selectedIndex === null || this.selectedIndex === 0) {
+          this.selectedIndex = this.matches.length - 1;
+        } else {
+          this.selectedIndex -= 1;
+        }
+        this.bestMatchIndex = null;
+        this.draw();
+        break;
+      }
+      case 'Tab': {
+        if (this.bestMatchIndex !== null && this.matches.length !== 0) {
+          this.input.value = this.matches[this.bestMatchIndex].name;
+          event.preventDefault();
+        }
+        this.reset();
+        break;
+      }
+      case 'Enter': {
+        this.reset();
+        break;
+      }
+      case 'Escape': {
+        this.input.value = this.previousValue;
+        this.reset();
+        break;
+      }
+    }
   },
 
   fetchMatches: function(query, callback) {
@@ -58,9 +103,14 @@ const Autocomplete = {
       this.overlay.textContent = '';
     }
 
-    this.matches.forEach(match => {
+    this.matches.forEach((match, index) => {
       let li = document.createElement('li');
       li.classList.add('autocomplete-ui-choice');
+
+      if (index === this.selectedIndex) {
+        li.classList.add('selected');
+        this.input.value = match.name;
+      }
 
       li.textContent = match.name;
       this.listUI.appendChild(li);
@@ -71,18 +121,23 @@ const Autocomplete = {
     this.visible = false;
     this.matches = [];
     this.bestMatchIndex = null;
+    this.selectedIndex = null;
+    this.previousValue = null;
 
     this.draw();
   },
 
   valueChanged: function() {
     let value = this.input.value;
+    this.previousValue = value;
 
     if (value.length > 0) {
       this.fetchMatches(value, matches => {
         this.visible = true;
         this.matches = matches;
         this.bestMatchIndex = 0;
+        this.selectedIndex = null;
+
         this.draw();
       });
     } else {
