3c90b8e74a039a8fb9e021f491b4599161a54450
EricRicketts <eric_ricketts@me.com>
2020-10-05 18:49:58 -0400

    exercises making http requests first exercise

--- exercises/Javascript/making_http_requests/booking_app_node/public/exercise_one.js
+++ exercises/Javascript/making_http_requests/booking_app_node/public/exercise_one.js
@@ -4,6 +4,34 @@ function addNumberOf(items, row) {
   let text = document.createTextNode(number.toString());
   cell.appendChild(text);
 }
+function numberOfSchedulesWithBookings(schedules, scheduleRow) {
+  let numberOfBookings = Array.from(schedules).filter(schedule => schedule["student_email"] !== null).length;
+  let text = document.createTextNode(numberOfBookings.toString());
+  let cell = scheduleRow.querySelector('td:nth-of-type(3)');
+  cell.appendChild(text);
+}
+
+function numberOfStaffWithSchedules(schedules, staffRow) {
+  let staffIds = Array.from(schedules).map(schedule => schedule["staff_id"]);
+  let uniqueIds = staffIds.reduce((ids, id) => {
+      if (!ids.includes(id)) { ids.push(id) };
+      return ids;
+  }, []);
+  let text = document.createTextNode(uniqueIds.length.toString());
+  let cell = staffRow.querySelector('td:nth-of-type(4)');
+  cell.appendChild(text);
+}
+
+function numberOfStudentsWithBookings(schedules, studentRow) {
+  let studentEmails = Array.from(schedules).map(schedule => schedule['student_email']).filter(email => email !== null);
+  let uniqueStudentEmails = studentEmails.reduce((emails, email) => {
+    if (!emails.includes(email)) { emails.push(email) };
+    return emails;
+  }, []);
+  let text = document.createTextNode(uniqueStudentEmails.length.toString());
+  let cell = studentRow.querySelector('td:nth-of-type(3)');
+  cell.appendChild(text);
+}
 
 function makeRequestForTotalNumberOf(item, row, DOMAIN) {
   let request = new XMLHttpRequest();
@@ -12,6 +40,16 @@ function makeRequestForTotalNumberOf(item, row, DOMAIN) {
   request.addEventListener('load', event => {
     let items = event.target.response;
     addNumberOf(items, row);
+    switch (item) {
+      case '/schedules': {
+        let staffRow = document.getElementById('staff');
+        let studentRow = document.getElementById('students');
+        numberOfSchedulesWithBookings(items, row);
+        numberOfStaffWithSchedules(items, staffRow);
+        numberOfStudentsWithBookings(items, studentRow);
+        break;
+      }
+    }
   });
   request.send();
 }
