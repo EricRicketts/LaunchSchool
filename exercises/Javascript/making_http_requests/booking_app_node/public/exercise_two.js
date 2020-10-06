ea4ca2e71e3ddb76596e1c1eff12c81e77a37e56
EricRicketts <eric_ricketts@me.com>
2020-10-06 19:19:28 -0400

    Exercsies Making HTTP Requests Exercise Two

--- exercises/Javascript/making_http_requests/booking_app_node/public/exercise_two.js
+++ exercises/Javascript/making_http_requests/booking_app_node/public/exercise_two.js
@@ -1,5 +1,12 @@
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 function updateTableWithStaffTotals(staffAndSchedulesTotals) {
   let tableBody = document.querySelector('table > tbody');
+  removeAllChildNodes(tableBody);
   Object.entries(staffAndSchedulesTotals).forEach(arr => {
     let tr = document.createElement('tr');
     for (let index = 0; index < 2; index += 1) {
@@ -15,18 +22,42 @@ function updateTableWithStaffTotals(staffAndSchedulesTotals) {
 document.addEventListener('DOMContentLoaded', () => {
   const API = 'http://localhost:3000/api';
   const SCHEDULES_PATH = '/schedules';
+  let button = document.querySelector('button');
 
-  let request = new XMLHttpRequest();
-  request.open('GET', API + SCHEDULES_PATH);
-  request.responseType = 'json';
-  request.addEventListener('load', event => {
-    let schedules = event.target.response;
-    let staffAndSchedulesTotals = Array.from(schedules).reduce((obj, schedule) => {
-      let staffId = schedule["staff_id"].toString();
-      obj[staffId] =  obj[staffId] + 1 || 1;
-      return obj;
-    }, {});
-    updateTableWithStaffTotals(staffAndSchedulesTotals);
+  button.addEventListener('click', () => {
+    let request = new XMLHttpRequest();
+    request.open('GET', API + SCHEDULES_PATH);
+    request.responseType = 'json';
+    request.timeout = 5000;
+    request.addEventListener('load', event => {
+      let schedules = event.target.response;
+      if (Array.from(schedules).length === 0) {
+        Swal.fire({
+          icon: 'info',
+          title: 'Information',
+          text: 'Successful request but the server had no data'
+        });
+      } else {
+        let staffAndSchedulesTotals = Array.from(schedules).reduce((obj, schedule) => {
+          let staffId = schedule["staff_id"].toString();
+          obj[staffId] =  obj[staffId] + 1 || 1;
+          return obj;
+        }, {});
+        updateTableWithStaffTotals(staffAndSchedulesTotals);
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: 'Data exists and has been inserted into the table'
+        });
+      }
+    });
+    request.addEventListener('timeout', event => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops',
+        text: 'A timeout occurred, please resubmit'
+      });
+    });
+    request.send();
   });
-  request.send();
 });
