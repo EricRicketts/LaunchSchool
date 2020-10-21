ebed70fcd82c64edb1fe4b893cfff5c5e58293dd
EricRicketts <eric_ricketts@me.com>
2020-10-21 14:33:59 -0400

    jquery events practice problems

---
+++ HTMLCSSJavascript/DOMAndAsynchronousProgrammingWithJavascript/JavascriptLibraries/PracticeProblemsjQueryEvents/code/practice_problems_jquery_events.js
@@ -0,0 +1,16 @@
+$(window).on('load', function() {
+  $('form').on('submit', event => {
+    event.preventDefault();
+    let character = $('#key').val();
+
+    $(document).off('keypress').on('keypress', event => {
+        if (event.key !== character) { return; }
+        $('a').trigger('click');
+    });
+  });
+
+  $('a').on('click', event => {
+    event.preventDefault();
+    $('#accordion').slideToggle();
+  });
+});
