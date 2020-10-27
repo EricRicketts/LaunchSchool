a602a99365ad21c217a4e3e21726043b18695e88
EricRicketts <eric_ricketts@me.com>
2020-10-26 20:26:48 -0400

    notes on loading HTML via XHR

--- HTMLCSSJavascript/DOMAndAsynchronousProgrammingWithJavascript/MakingHTTPRequestsFromJavascript/example_loading_html_via_xhr/code/second_example.js
+++ HTMLCSSJavascript/DOMAndAsynchronousProgrammingWithJavascript/MakingHTTPRequestsFromJavascript/example_loading_html_via_xhr/code/second_example.js
@@ -15,7 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let path = event.target.getAttribute('href');
     let xhr = new XMLHttpRequest();
     xhr.open('GET', DOMAIN + path);
-    xhr.addEventListener('load', () => store.innerHTML = xhr.responseText);
+    xhr.addEventListener('load', event => store.innerHTML = event.target.responseText);
     xhr.send();
   });
 });
+
+/*
+ Each product in our final example has an edit form that you access by clicking first on the item link, and
+ then on "Edit this product." The code, though, incorrectly handles the form when you submit it.
+
+1. What prevents the form submission from working? Investigate the problem with the web developer tools in
+your browser.
+
+this happens because the form after being filled out is submitted via a normal POST request, but the URL for the
+POST request is incorrect, file:///products/1 (actually products/1 or products/2 or products/3).  This happened
+because it followed the default browser behavior and just took that URL from the form, which turned out to be
+incorrect.
+*/
