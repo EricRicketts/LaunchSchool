e1eee0fef337584e08550731bda936d2f8a33340
EricRicketts <eric_ricketts@me.com>
2020-11-22 08:02:41 -0500

    Javascript Exercises Front End Development With Javascript Making HTTP Requests Exercise Seven DELETE Schedule Working

---
+++ exercises/Javascript/making_http_requests/booking_app_node/public/javascripts/exercise_seven.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const schedulesAPI = 'http://localhost:3000/api/schedules';
+  const bookingsAPI = 'http://localhost:3000/api/bookings';
+  let schedulesForm = document.getElementById('cancel-schedule');
+  let bookingsForm = document.getElementById('cancel-booking');
+
+  schedulesForm.addEventListener('submit', event => {
+    event.preventDefault();
+    let schedule_id = document.querySelector('input[name="schedule_id"]').value;
+
+    let schedulesXhr = new XMLHttpRequest();
+    schedulesXhr.open('DELETE', `${schedulesAPI}/${schedule_id}`);
+    schedulesXhr.send();
+
+    schedulesXhr.addEventListener('load', event => {
+      let statusCode = event.target.status;
+      let responseMessage = event.target.responseText;
+
+      if (statusCode === 403 || statusCode === 404) {
+        alert(`${responseMessage}`);
+      } else if (statusCode === 204) {
+        alert(`Schedule with id ${schedule_id} successfully deleted.`);
+      } else {
+        return;
+      }
+    });
+  })
+});
