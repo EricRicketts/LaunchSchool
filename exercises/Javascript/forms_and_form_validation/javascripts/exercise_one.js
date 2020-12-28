33590ee84cc8c3a7137d3bb9a82dafd33331c98f
EricRicketts <eric_ricketts@me.com>
2020-12-28 15:15:52 -0500

    Exercises Forms And Form Validation Individual Input Validation Working

--- exercises/Javascript/forms_and_form_validation/javascripts/exercise_one.js
+++ exercises/Javascript/forms_and_form_validation/javascripts/exercise_one.js
@@ -1,26 +1,63 @@
-function attachRequiredEventHandler(form, nonRequiredElement) {
-
+function attachRequiredEventHandlers(form) {
   form.addEventListener('focusin', event => {
-    event.target.classList.remove('input-error');
-    let span = event.target.nextElementSibling;
-    span.classList.remove('show-error');
-    span.classList.add('hide-error');
+    if (event.target.nodeName === 'INPUT') {
+      event.target.classList.remove('input-error');
+      let span = event.target.nextElementSibling;
+      let secondSpan;
+      if (span.nextElementSibling) { secondSpan = span.nextElementSibling; }
+
+      span.classList.remove('show-error');
+      span.classList.add('hide-error');
+      if (secondSpan) {
+        secondSpan.classList.remove('show-error');
+        secondSpan.classList.add('hide-error');
+      }
+    }
   });
 
   form.addEventListener('focusout', event => {
-    let span = event.target.nextElementSibling;
-    if (event.target === nonRequiredElement) {
-      return;
-    } else if (event.target.validity.valueMissing) {
-      span.classList.remove('hide-error');
-      span.classList.add('show-error');
-      event.target.classList.add('input-error');
+    if (event.target.nodeName === 'INPUT') {
+      let span = event.target.nextElementSibling;
+      let secondSpan;
+      if (span.nextElementSibling) { secondSpan = span.nextElementSibling; }
+
+      if (event.target.validity.valid) {
+        removeAllErrors(event, span, secondSpan)
+      } else if (event.target.validity.valueMissing && event.target.id !== 'phone') {
+        showFirstSpanError(span, event)
+      } else if (event.target.validity.tooShort) {
+        showSecondSpanError(span, secondSpan, event);
+      } else if (event.target.validity.patternMismatch && event.target.id === 'email') {
+        showSecondSpanError(span, secondSpan, event);
+      } else if (event.target.validity.patternMismatch && event.target.id === 'phone') {
+        showFirstSpanError(span, event);
+      }
     }
   });
 }
+function removeAllErrors(event, span, secondSpan) {
+  event.target.classList.remove('input-error');
+  span.classList.remove('show-error');
+  span.classList.add('hide-error');
+  if (secondSpan) {
+    secondSpan.classList.remove('show-error');
+    secondSpan.classList.add('hide-error');
+  }
+}
+function showFirstSpanError(span, event) {
+  span.classList.remove('hide-error');
+  span.classList.add('show-error');
+  event.target.classList.add('input-error');
+}
+function showSecondSpanError(span, secondSpan, event) {
+  span.classList.remove('show-error');
+  span.classList.add('hide-error');
+  secondSpan.classList.remove('hide-error');
+  secondSpan.classList.add('show-error');
+  event.target.classList.add('input-error');
+}
 document.addEventListener('DOMContentLoaded', function() {
   let form = document.querySelector('form');
-  let phone = document.getElementById('phone');
 
-  attachRequiredEventHandler(form, phone);
+  attachRequiredEventHandlers(form);
 });
