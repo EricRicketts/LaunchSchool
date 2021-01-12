de5e550159df82637866bd93908a607c133e45db
EricRicketts <eric_ricketts@me.com>
2021-01-12 18:02:31 -0500

    Exercise Three Miscellaneous GUI Apps

--- exercises/Javascript/miscellaneous_gui_apps/context_menu_for_todo_list/app.js
+++ exercises/Javascript/miscellaneous_gui_apps/context_menu_for_todo_list/app.js
@@ -26,13 +26,54 @@ let App = {
   listItemHandler: function(event, listItem) {
     event.preventDefault();
     this.showMenu(event, listItem);
+    let menu = listItem.querySelector('div.menu');
+    let menuParagraphs = Array.from(menu.querySelectorAll('p'));
+    menuParagraphs.forEach(p => p.addEventListener('mouseenter', event => this.menuEnterHandler(event)));
+    menuParagraphs.forEach(p => p.addEventListener('mouseleave', event => this.menuExitHandler(event)));
+    menuParagraphs.forEach(p => p.addEventListener('click', event => this.menuClickHandler(event)));
     this.document.addEventListener('click', this.clickOutsideMenuHandler.bind(this));
+  },
+  menuClickHandler: function(event) {
+    event.target.parentElement.style.display = 'none';
+    if (event.target.classList.contains('delete_todo')) {
+      let id = event.target.dataset.id;
+      this.popUp.dataset.id = id;
+      this.popUp.style.display = 'block';
+      this.yesButton.addEventListener('click', event => this.yesButtonHandler(event));
+      this.noButton.addEventListener('click', event => this.hidePopup(event));
+    }
+  },
+  menuEnterHandler: function(event) {
+    event.target.style.backgroundColor = 'darkgray';
+    event.target.style.color = 'white';
+  },
+  menuExitHandler: function(event) {
+    event.target.style.backgroundColor = 'lightgray';
+    event.target.style.color = 'black';
+  },
+  noButtonEventHandler: function(event) {
+    this.hidePopup(event);
+    event.target.removeEventListener('click', this.noButtonEventHandler);
   },
   populateTodos: function() {
     this.todoItems.forEach(({ id, title }) => {
       let html = this.template({ id, title });
       this.list.insertAdjacentHTML('beforeend', html);
     })
+  },
+  removeTodoItem: function(event, id) {
+    let listItemToRemove = Array.from(this.list.querySelectorAll('li')).find(li => li.dataset.id === id.toString());
+    if (listItemToRemove) {
+      this.list.removeChild(listItemToRemove);
+      this.hidePopup(event);
+    }
+  },
+  removeTodoItemData(id) {
+    let todoItemToRemove = this.todoItems.find(todo => todo.id === id);
+    if (todoItemToRemove) {
+      let indexToRemove = this.todoItems.findIndex(todo => todo.id === todoItemToRemove.id);
+      this.todoItems.splice(indexToRemove, 1);
+    }
   },
   showMenu: function(event, listItem) {
     let menu = listItem.querySelector('div.menu');
@@ -42,35 +83,20 @@ let App = {
     menu.style.left = `${x}px`;
     menu.style.top = `${y}px`;
     menu.style.display = 'block';
-    let menuParagraphs = Array.from(menu.querySelectorAll('p'));
-    menuParagraphs.forEach(p => {
-      p.addEventListener('mouseenter', event => {
-        event.target.style.backgroundColor = 'darkgray';
-        event.target.style.color = 'white';
-      });
-    });
-    menuParagraphs.forEach(p => {
-      p.addEventListener('mouseleave', event => {
-        event.target.style.backgroundColor = 'lightgray';
-        event.target.style.color = 'black';
-      });
-    });
   },
-  yesButtonHandler: function(event, id, element) {
-    let numericId = Number.parseInt(id, 10);
-    let todoItemToDelete = this.todoItems.find(obj => obj.id === numericId);
-    if (todoItemToDelete) {
-      let indexToDelete = this.todoItems.findIndex(obj => obj.id === todoItemToDelete.id);
-      this.todoItems.splice(indexToDelete, 1);
-      this.list.removeChild(element);
-      this.hidePopup(event);
-    }
+  yesButtonHandler: function(event) {
+    let numericId = Number.parseInt(event.target.parentElement.parentElement.dataset.id, 10);
+    this.removeTodoItemData(numericId);
+    this.removeTodoItem(event, numericId);
   },
   init: function(document) {
     let source = document.getElementById('todo_items').innerHTML;
     this.template = Handlebars.compile(source);
     this.document = document;
     this.list = this.document.getElementById('todos');
+    this.popUp = this.document.getElementById('popup');
+    this.yesButton = this.popUp.querySelectorAll('button').item(0);
+    this.noButton = this.popUp.querySelectorAll('button').item(1);
     this.todoItems = JSON.parse(JSON.stringify(todoItems));
     this.populateTodos();
     this.addListItemHandlers();
@@ -79,14 +105,3 @@ let App = {
 document.addEventListener('DOMContentLoaded', function() {
   App.init(document);
 });
-
-/*
-  1.  Start with a right click on the li element, this needs to bring up the menu.
-  2.  put a mouseover event on the delete selection
-      a.  If there is a mouseover, then get the parent and change the color and text.
-      b.  If there is a 'click' event the menu needs to disappear and the popup needs to be enabled
-      b.  If there is a 'click' on the delete element then get the data-id of the parent div.
-      c.  If there is a 'click' on the delete element then use the data-id to find the li element to delete it.
-      d.  if there is a mouseleave event then change the color and text back to the default.
-      e.  if there is a document click event then if the menu is not hidden it needs to be removed
-*/
